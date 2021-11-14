cec7a8e14007c8dc4a00bd8024b421c79196f8b9
Nildo <nildo@webstack.com.br>
2021-11-14 17:17:44 -0300

    declararVariaveis.js atualizado.

---
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/declararVariaveis.js
@@ -0,0 +1,3 @@
+var minhaVariavel; // declarando uma variável com a palavra chave var
+let cor; // declarando uma variável com a palavra chave let
+const idade; // declarando uma variável com a palavra chave const
