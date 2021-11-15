86014400d6a06d6ac04de1459147fb1aba1ef8e8
Nildo <nildo@webstack.com.br>
2021-11-14 22:47:35 -0300

    variavesNaoInicializadas.js atualizado.

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/variavesNaoInicializadas.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/variavesNaoInicializadas.js
@@ -1,3 +1,11 @@
+/**
+    Quando as variáveis de JavaScript são declaradas, elas têm um valor inicial
+    de undefined. Se você fizer uma operação matemática em uma variável
+    undefined, seu resultado será NaN, o que significa que "Não é um número". Se
+     você concatenar uma string com uma variável undefined, você receberá uma
+     string literal de undefined.
+*/
+
 var a = 5;
 var b = 10;
 var c = 'I am a';
