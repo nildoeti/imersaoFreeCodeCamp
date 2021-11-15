e616634cbdb2e4a9409bcfec42f11b22b0d65359
Nildo <nildo@webstack.com.br>
2021-11-15 11:27:59 -0300

    adicioanarDoisNumeros.js foi atualizado.

---
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/adicioanarDoisNumeros.js
@@ -0,0 +1,19 @@
+/*
+    Adicionar dois números com JavaScript
+
+    Number é um tipo de dado em JavaScript que representa um dado numérico.
+
+    Agora, vamos tentar adicionar dois números usando JavaScript.
+
+    JavaScript utiliza o símbolo + como um operador de adição quando colocado entre dois números.
+
+    Exemplo:
+
+    const myVar = 5 + 10;
+
+    myVar agora tem o valor de 15.
+
+*/
+
+const soma = 10 + 10;
+console.log(soma);
