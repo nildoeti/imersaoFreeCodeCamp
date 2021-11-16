c478b780c49e9989aefb71b34329ddf2ce3079aa
Nildo <nildo@webstack.com.br>
2021-11-15 22:39:49 -0300

    resto.js atualizado.

---
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/resto.js
@@ -0,0 +1,8 @@
+ /*
+    O operador de resto % retorna o resto da divisão de dois números.
+    Na matemática, um número pode ser verificado como par ou ímpar por meio do
+    resto da divisão do número por 2.
+ */
+
+console.log(17 % 2); // 17 é ímpar pois tem resto 1
+console.log(48 % 2); // 48 é par pois tem resto 0
