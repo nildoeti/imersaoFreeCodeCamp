0e39089c43b6e631f4346aa0a4245d3e41c15e5d
Nildo <nildo@webstack.com.br>
2021-11-19 16:39:27 -0300

    divisao-aumentada.js foi atualizado.

---
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/divisao-aumentada.js
@@ -0,0 +1,22 @@
+/*
+    Atribuir de modo composto com divisão aumentada
+
+    O operador /= divide uma variável por outro número.
+
+    myVar = myVar / 5;
+
+    dividirá myVar por 5. Essa expressão pode ser reescrita assim:
+
+    myVar /= 5;
+*/
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Converter as atribuições para a, b e c para usar o operador /=.
+a /= 12; // 4
+b /= 4; // 27
+c /= 11; // 3
+
+console.log(a, b, c);
