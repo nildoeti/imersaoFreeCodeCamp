ec46e69b3d552043355c5a617e71e9cebe57e1b0
Nildo <nildo@webstack.com.br>
2021-11-19 16:34:09 -0300

    multiplicacaoAumentada.js foi atualizado.

---
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/multiplicacaoAumentada.js
@@ -0,0 +1,22 @@
+/*
+    Atribuir de modo composto com multiplicação aumentada
+
+    O operador *= multiplica uma variável por um número.
+
+    myVar = myVar * 5;
+
+    multiplicará myVar por 5. Essa expressão pode ser reescrita assim:
+
+    myVar *= 5;
+
+*/
+
+let a = 5;
+let b = 12;
+let c =  4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+console.log(a, b, c);
