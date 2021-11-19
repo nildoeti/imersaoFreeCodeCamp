328624c4a6100112931d263293637320fa869373
Nildo <nildo@webstack.com.br>
2021-11-18 22:57:22 -0300

    subtracaoAumentada.js atualizado.

---
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/subtracaoAumentada.js
@@ -0,0 +1,21 @@
+/*
+    Atribuir de modo composto com subtração aumentada
+
+    Como o operador +=, -= subtrai um número de uma variável.
+
+    myVar = myVar - 5;
+
+    vai subtrair 5 de myVar. Essa expressão pode ser reescrita assim:
+
+    myVar -= 5;
+*/
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+console.log(a, b, c);
