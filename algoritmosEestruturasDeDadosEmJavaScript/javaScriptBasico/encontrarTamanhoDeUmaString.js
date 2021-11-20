bd56dd1d3047d31b00a36f99340c05652c38d5b7
Nildo <nildo@webstack.com.br>
2021-11-20 17:42:16 -0300

    encontrarTamanhoDeUmaString.js foi atualizado.

---
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/encontrarTamanhoDeUmaString.js
@@ -0,0 +1,24 @@
+/*
+    Encontrar o tamanho de uma string
+
+    Você pode encontrar o tamanho de um valor de String ao escrever .length após
+    a variável de string ou literal de string.
+
+    console.log("Alan Peter".length);
+
+    O valor 10 seria exibido no console.
+
+    Por exemplo, se nós criássemos uma variável const firstName = "Ada",
+    poderíamos descobrir qual o tamanho da string Ada usando a propriedade
+    firstName.length.
+
+    Use a propriedade .length para contar o número de caracteres na variável
+    lastName e atribui-la a lastNameLength.
+
+*/
+
+let lastNameLength = 0;
+const lastName = 'Lovelace';
+lastNameLength = lastName.length;
+
+console.log(lastNameLength);
