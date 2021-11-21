27d4818ca0e9eb8e9d14e16fdeab50f8427c6037
Nildo <nildo@webstack.com.br>
2021-11-21 11:36:43 -0300

    aninharUmArrayEmOutroArray.js foi atualizado.

---
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/aninharUmArrayEmOutroArray.js
@@ -0,0 +1,12 @@
+/*
+    Aninhar um array em outro array
+
+    Você também pode aninhar arrays dentro de outros arrays, como abaixo:
+
+    const teams = [["Bulls", 23], ["White Sox", 45]];
+
+    Isso é chamado um array multidimensional.
+*/
+
+const myArray = [[1, 5], ['dog', 'cat']];
+console.log(myArray);
