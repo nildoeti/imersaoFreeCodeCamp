d0a20c384dbfa87d4627b2d0018ee9fc6b62edc9
Nildo <nildo@webstack.com.br>
2021-11-23 12:08:25 -0300

    manipularArraysComPop.js foi atualizado.

---
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/manipularArraysComPop.js
@@ -0,0 +1,29 @@
+/*
+	Manipular arrays com pop()
+
+	Outra forma de alterar os dados em um array é com a função .pop().
+
+	.pop() é usado para remover um valor do final do array. Nós podemos 
+	armazenar esse valor removido atribuindo-o a uma variável. Em outras 
+	palavras, .pop() remove o último elemento de um array e retorna aquele 
+	elemento.
+
+	Qualquer tipo de entrada pode ser removida de um array - numbers, strings 
+	e até mesmo arrays aninhados.
+
+	const threeArr = [1, 4, 6];
+	const oneDown = threeArr.pop();
+	console.log(oneDown);
+	console.log(threeArr);
+
+	O primeiro console.log exibirá o valor 6 e o segundo exibirá o valor [1, 4].
+
+	Use a função .pop() para remover o último item de myArray e atribuir o valor 
+	removido para uma nova variável, removedFromMyArray.
+*/
+
+const threeArr = [1, 4, 6];
+const oneDown = threeArr.pop();
+
+console.log(threeArr); // [ 1, 4 ]
+console.log(oneDown); // 6
