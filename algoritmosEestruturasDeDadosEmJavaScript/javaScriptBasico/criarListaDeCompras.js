ad1d4d97d680e3e45b3c7c0a72fc09875f8cdc26
Nildo <nildo@webstack.com.br>
2021-11-25 19:47:02 -0300

    criarListaDeCompras.js foi atualizado.

---
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/criarListaDeCompras.js
@@ -0,0 +1,24 @@
+/*
+	Criar lista de compras
+
+	Crie uma lista de compras na variável myList. A lista deve ser um array 
+	multidimensional contendo diversos sub-arrays.
+
+	O primeiro elemento em cada sub-array deve conter uma string com o nome do 
+	item. O segundo elemento deve ser um número representando a quantidade, ou 
+	seja,
+
+	["Chocolate Bar", 15]
+
+	Deve haver pelo menos 5 sub-arrays na lista.
+*/
+
+const myList = [
+	['Arroz Camil tipo 1', 5],
+	['Feijão Camil tipo 1', 3],
+	['Açucar União', 7],
+	['Macarrão Adria tipo parafuso', 4],
+	['Biscoito Trackinas Chocolate', 7 ]
+];
+
+console.log(myList[0][1]);
