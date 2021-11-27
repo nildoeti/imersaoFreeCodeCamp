bb39f40d03bd97b8c6e3755162655f7fe1b585d4
Nildo <nildo@webstack.com.br>
2021-11-26 21:00:42 -0300

    entenderValorUndefinedRetornadoDeUmaFuncao.js foi atualizado.

---
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/entenderValorUndefinedRetornadoDeUmaFuncao.js
@@ -0,0 +1,50 @@
+/*
+	Entender o valor undefined retornado de uma função
+
+	Uma função pode incluir a instrução return mas ela não precisa fazer isso. 
+	No caso de a função não ter uma instrução return, quando você a chamar, a 
+	função processa o código interno, mas o valor retornado é undefined.
+
+	Exemplo
+
+	let sum = 0;
+
+	function addSum(num) {
+	  sum = sum + num;
+	}
+
+	addSum(3);
+
+	addSum é uma função sem uma instrução return. A função vai alterar a 
+	variável global sum, mas o valor retornado da função é undefined.
+
+	Crie uma função addFive sem qualquer argumento. Essa função adiciona 5 à 
+	variávelsum, mas o valor retornado é undefined.
+*/
+
+// exemplo 1
+let sum = 0;
+
+function addSum(num) {
+	// função sem a instrução return...
+	sum += sum + num;
+}
+
+addSum(3); 
+
+
+// exemplo 2
+let sum = 0;
+
+function addThree() {
+	// body...
+	sum  = sum + 3;
+}
+
+function addFive() {
+	// body...
+	sum += 5;
+}
+
+addThree();
+addFive();
