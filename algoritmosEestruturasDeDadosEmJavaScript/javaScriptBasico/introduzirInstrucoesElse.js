322f138f46f50d98246f0ef7dbf49801a1d85580
nildoeti <nildo@webstack.com.br>
2021-12-23 09:23:29 -0300

    Exemplo para condicional ELSE adicionado.

---
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/introduzirInstrucoesElse.js
@@ -0,0 +1,22 @@
+/*
+	Introduzir instruções else
+
+	Quando uma condição para uma instrução if for verdadeira, o bloco de código 
+	seguinte será executado. E quando a condição for falsa? Normalmente, nada 
+	aconteceria. Com uma instrução else, um bloco de código alternativo pode 
+	ser executado.
+
+	if (num > 10) {
+	  return "Bigger than 10";
+	} else {
+	  return "10 or Less";
+	}
+*/
+
+// exemplo 1
+if (num > 10) {
+	return 'Bigger than 10';
+} else {
+	return '10 or less';
+}
+
