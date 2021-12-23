c5e58e43f896bf67a24e356a49147eaf22367477
nildoeti <nildo@webstack.com.br>
2021-12-23 09:36:26 -0300

    Adicioando exemplo de uso da condicional if/else encadeados.

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/introduzirInstrucoesElseIf.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/introduzirInstrucoesElseIf.js
@@ -1,1 +1,26 @@
-ntroduzirInstrucoesElseIf.js
+/*
+	Introduzir instruções else if
+
+	Se você tem múltiplas condições que precisam ser resolvidas, você pode 
+	encadear as instruções if junto com instruções else if.
+
+	if (num > 15) {
+	  return "Bigger than 15";
+	} else if (num < 5) {
+	  return "Smaller than 5";
+	} else {
+	  return "Between 5 and 15";
+	}
+*/
+
+// if encadeado
+let num = 23;
+
+if (num > 15) {
+	return 'Bigger than 15';
+} else if (num < 5) {
+	return 'Smaller than 5';
+} else {
+	return 'Between 5 and 15'
+}
+
