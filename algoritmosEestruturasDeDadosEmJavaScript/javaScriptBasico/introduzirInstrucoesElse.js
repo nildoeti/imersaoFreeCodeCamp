2197faf6b501f8faf543b7f48a02a1466820473a
nildoeti <nildo@webstack.com.br>
2021-12-23 09:30:27 -0300

    Exemplo para condicional ELSE com uso de função foi adicionado.

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/introduzirInstrucoesElse.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/introduzirInstrucoesElse.js
@@ -14,9 +14,23 @@
 */
 
 // exemplo 1
-if (num > 10) {
-	return 'Bigger than 10';
-} else {
-	return '10 or less';
+// let num = 3;
+
+// if (num > 10) {
+// 	return 'Bigger than 10';
+// } else {
+// 	return '10 or less';
+// }
+
+// exemplo prático com uso de funções
+function testLogicalElse(val) {
+	let result = '';
+	if (val > 5) {
+		return 'Bigger than 5';
+	} else {
+		return '5 or Smaller'
+	}
 }
 
+const result = testLogicalElse(43);
+console.log(result);
