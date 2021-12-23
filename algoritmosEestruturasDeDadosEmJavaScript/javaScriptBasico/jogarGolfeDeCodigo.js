31313d8e0c6837b88759953201648eafcab904ad
Nildo <nildo@webstack.com.br>
2021-12-23 12:03:46 -0300

    Adicioando segunda soluçao.

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/jogarGolfeDeCodigo.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/jogarGolfeDeCodigo.js
@@ -57,3 +57,28 @@ function golfScore(par, strokes) {
 	}
 
 }
+
+const result = golfScore(5, 7);
+console.log(result);
+
+// examplo 2
+function golfScore1(par, strokes) {
+	if (strokes == 1) {
+		return names[0]
+	} else if (strokes <= par -2) {
+		return names[1];
+	} else if (strokes == par -1) {
+		return name[2];
+	} else if (strokes == par) {
+		return names[3];
+	} else if (strokes == par + 1) {
+		return names[4];
+	} else if (strokes == par + 2) {
+		return names[5];
+	} else if (strokes == par + 3) {
+		return names[6];
+	}
+}
+
+const result1 = golfScore1(5, 7);
+console.log(result1)
