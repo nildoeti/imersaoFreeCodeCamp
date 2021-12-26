4a9fb99510e1937e650a099b244bb2b7bc912c79
Nildo <nildo@webstack.com.br>
2021-12-25 23:10:15 -0300

    Gerado nova atualização para swith/case

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/substituirCadeiasIfElsePorSwitch.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/substituirCadeiasIfElsePorSwitch.js
@@ -76,31 +76,31 @@
 // 	}
 // }
 
-const result = chainToSwitch(1);
-console.log(result);
+// const result = chainToSwitch(1);
+// console.log(result);
 
 // exemplo de chainToSwitch com switch
 const chainToSwitch = (val) => {
 	let answer = '';
 
-	switch(val) {
-		// case: 'bob';
-		// 	answer = Marley;
-		// 	break;
-		case: 42;
-			answer = 'The answer';
-			break;
-		case: 1;
-			answer = 'There is no #1';
-			break;
-		case 99: 
-			answer = 'Missid me by this mush';
-			break;
-		case: 7:
-			answer = 'Ate Nine'; 
+	switch (val) {
+  case "bob":
+    answer = "Marley";
+    break;
+  case 42:
+    answer = "The Answer";
+    break;
+  case 1:
+    answer = "There is no #1";
+    break;
+  case 99:
+    answer = "Missed me by this much!";
+    break;
+  case 7:
+    answer = "Ate Nine";
+    break;
 	}
-	return answer;
 }
 
-// const result = chainToSwitch(1);
-// console.log(result);
+const result = chainToSwitch(1);
+console.log(result);
