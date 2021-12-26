29db2c8f2aa70eef270cb9bb4dc83edf9783a9a5
Nildo <nildo@webstack.com.br>
2021-12-25 23:04:45 -0300

    Adicionado exemplo com uso de switch/case

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/substituirCadeiasIfElsePorSwitch.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/substituirCadeiasIfElsePorSwitch.js
@@ -43,16 +43,64 @@
 // }
 
 // pode ser substituído por:
-function myFun(val) {
+// function myFun(val) {
+// 	let answer = '';
+// 	switch(val) {
+// 		case 1:
+// 			answer = 'a';
+// 			break;
+// 		case 2:
+// 			answer = 'b';
+// 			break;
+// 		default:
+// 			answer = 'c';
+// 	}
+// }
+
+
+// exemplo de função com else/if 
+// function chainToSwitch(val) {
+// 	let answer = '';
+// 	if (val === 'bob') {
+// 		answer = 'Marley';
+// 	} else if (val === 42) {
+// 		answer = 'The answer';
+// 	} else if (val === 1) {
+// 		answer = 'There is no #1';
+// 	} else if (val === 99) {
+// 		answer = 'Missed me by this mush.';
+// 	} else if (val === 9) {
+// 		answer = 'By Bob';
+// 	} else {
+// 		answer = 'Good By.';
+// 	}
+// }
+
+const result = chainToSwitch(1);
+console.log(result);
+
+// exemplo de chainToSwitch com switch
+const chainToSwitch = (val) => {
 	let answer = '';
+
 	switch(val) {
-		case 1:
-			answer = 'a';
+		// case: 'bob';
+		// 	answer = Marley;
+		// 	break;
+		case: 42;
+			answer = 'The answer';
 			break;
-		case 2:
-			answer = 'b';
+		case: 1;
+			answer = 'There is no #1';
 			break;
-		default:
-			answer = 'c';
+		case 99: 
+			answer = 'Missid me by this mush';
+			break;
+		case: 7:
+			answer = 'Ate Nine'; 
 	}
+	return answer;
 }
+
+// const result = chainToSwitch(1);
+// console.log(result);
