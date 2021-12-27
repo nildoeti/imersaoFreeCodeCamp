242176ce2678c4a09f90fb2775cda8e169cdf494
Nildo <nildo@webstack.com.br>
2021-12-27 20:08:47 -0300

    Adicioando exemplo 3 ao projeto.

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/acessarPropriedadesDeObjetosComVariaveis.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/acessarPropriedadesDeObjetosComVariaveis.js
@@ -69,3 +69,15 @@ function propFix(str) {
 const someProp = propFix('Name');
 const result = someObj[someProp];
 console.log(result);
+
+
+// exemplo 3
+const testObj = {
+	12: 'Namath',
+	16: 'Montana',
+	19: 'Unitas'
+};
+
+const playerNumber = 16;
+const player = testObj[playerNumber];
+console.log(player);
