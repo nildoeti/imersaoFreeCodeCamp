230093590169f3eaa7b9a878fb46229b1bc8512a
Nildo <nildo@webstack.com.br>
2021-12-27 10:32:02 -0300

    Adicionado exemplos do exercício.

---
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/criarObjetosJavaScript.js
@@ -0,0 +1,65 @@
+/*
+	Criar objetos JavaScript
+
+	Você talvez tenha ouvido o termo objeto antes.
+
+	Objetos são similares a arrays, exceto que, ao invés de usar índices para 
+	acessar e modificar seus dados, você acessa os dados em objetos através do 
+	que se chama propriedades.
+
+	Objetos são úteis para armazenar dados de forma estruturada e podem 
+	representar objetos do mundo real, como um gato.
+
+	Aqui está um exemplo de objeto gato:
+
+	const cat = {
+	  "name": "Whiskers",
+	  "legs": 4,
+	  "tails": 1,
+	  "enemies": ["Water", "Dogs"]
+	};
+
+	Neste exemplo, todas as propriedades são armazenadas como strings, como 
+	name, legs e tails. Porém, você também pode usar números como propriedades. 
+	Você pode até omitir as aspas para propriedades de string com uma única 
+	palavra, da seguinte forma:
+
+	const anotherObject = {
+	  make: "Ford",
+	  5: "five",
+	  "model": "focus"
+	};
+
+	No entanto, se seu objeto tem quaisquer propriedades que não sejam strings, 
+	o JavaScript automaticamente definirá seus tipos como strings.
+
+*/
+
+// exemplo 1
+const cat = {
+	'name': 'Whiskers',
+	'legs': 4,
+	'tails': 1,
+	'enemies': ['Water', 'Dogs']
+};
+
+console.log(cat);
+
+// exemplo 2
+const cats = {
+	name: 'Whiskers',
+	legs: 4,
+	tails: 1,
+	enemies: ['Water', 'Dogs']
+};
+
+console.log(cats);
+
+// exemplo 3
+const anotherObject = {
+	make: 'Ford',
+	5: 'five',
+	'model': 'focus'
+};
+
+console.log(anotherObject);
