7418ee3b0af85224dba92a10d51aca9e15dbab58
Nildo <nildo@webstack.com.br>
2021-12-28 19:38:32 -0300

    Adicionado exemplo do desafio

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/adicionarNovasPropriedadesParaUmObjetoJavaScript.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/adicionarNovasPropriedadesParaUmObjetoJavaScript.js
@@ -41,3 +41,23 @@ console.log(result);
 ourDog.bark = 'bow-wow';
 console.log(ourDog);
 
+
+/**
+ * Desafio
+ * Adicione a propriedade bark para myDog e defina-a para um som de um cachorro, 
+ * como "woof". Você pode usar tanto notação de ponto quando de colchetes.
+ * 
+ */
+
+const myDog = {
+    'name': 'Happy Coder',
+    'legs': 4,
+    'tails': 1,
+    'friends': ['freeCodeCamp Campers']
+};
+
+console.log('Antes:\n', myDog);
+
+// depois
+myDog.bark = 'woof';
+console.log('Depois:\n', myDog);
