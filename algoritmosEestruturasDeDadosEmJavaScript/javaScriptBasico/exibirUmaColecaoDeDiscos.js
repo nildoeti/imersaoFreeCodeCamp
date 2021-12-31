063246635c95d3a34e1bc8450ce61757fde793d7
Nildo <nildo@webstack.com.br>
2021-12-31 15:44:07 -0300

    Adcionado nova solução para o desafio

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/exibirUmaColecaoDeDiscos.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/exibirUmaColecaoDeDiscos.js
@@ -24,42 +24,80 @@
 	Observação: uma cópia do objeto recordCollection é usada para testes.
 */
 
-const recordCollection = {
-	2548: {
-		albumTitle: 'Slippery When Wet',
-		artist: 'Bom Jovi',
-		tracks: ['Let it rock', 'You give love a bad name']
-	},
+// const recordCollection = {
+// 	2548: {
+// 		albumTitle: 'Slippery When Wet',
+// 		artist: 'Bom Jovi',
+// 		tracks: ['Let it rock', 'You give love a bad name']
+// 	},
 
-	2468: {
-		albumTitle: '1999',
-		artist: 'Prince',
-		tracks: ['1999', 'Litte red covertte']
-	},
+// 	2468: {
+// 		albumTitle: '1999',
+// 		artist: 'Prince',
+// 		tracks: ['1999', 'Litte red covertte']
+// 	},
 
-	1245: {
-		artist: 'Robert Palmer',
-		tracks: []
-	},
+// 	1245: {
+// 		artist: 'Robert Palmer',
+// 		tracks: []
+// 	},
 
-	5439: {
-		albumTitle: 'ABBA gold'
-	}
+// 	5439: {
+// 		albumTitle: 'ABBA gold'
+// 	}
+// };
+
+// function updateRecords(records, id, prop, value) {
+// 	// body...
+// 	if (prop !== 'tracks' && value) {
+// 		records[id][prop] = value;
+// 	} else if (prop === 'tracks' && records[id].hasOwnProperty('tracks') === false) {
+// 		records[id][prop] = [value]
+// 	} else if (prop === 'tracks' && value !== '') {
+// 		records[id][prop].push(value);
+// 	} else if (value === '') {
+// 		delete records[id][prop];
+// 	}
+
+// 	return records;
+// }
+
+// const result1 = updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+// console.log(result1);
+
+
+// solução 2
+var recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
 };
 
+// Only change code below this line
 function updateRecords(records, id, prop, value) {
-	// body...
-	if (prop !== 'tracks' && value) {
-		records[id][prop] = value;
-	} else if (prop === 'tracks' && records[id].hasOwnProperty('tracks') === false) {
-		records[id][prop] = [value]
-	} else if (prop === 'tracks' && value !== '') {
-		records[id][prop].push(value);
-	} else if (value === '') {
-		delete records[id][prop];
-	}
-
-	return records;
+  if (value === '') {
+    delete records[id][prop];
+  } else if (prop === 'tracks') {
+    records[id][prop] = records[id][prop] || []; // this is called shortcircuit evaluation, see below for explanation
+    records[id][prop].push(value);
+  } else {
+    records[id][prop] = value;
+  }
+  return records;
 }
 
 const result1 = updateRecords(recordCollection, 5439, 'artist', 'ABBA');
