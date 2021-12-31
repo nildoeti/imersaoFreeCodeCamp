ff6345dbca1594cb5661d7dfe53ff8adc462c4dd
Nildo <nildo@webstack.com.br>
2021-12-31 15:39:13 -0300

    Finalizado desafio do projeto.

---
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/exibirUmaColecaoDeDiscos.js
@@ -0,0 +1,66 @@
+/*
+	Exibir uma coleção de discos
+
+	Você recebeu um objeto literal representando uma parte da sua coleção de 
+	álbuns musicais. Cada álbum possui um número de id único como sua chave e 
+	diversas outras propriedades. Nem todos os álbuns possuem informações 
+	completas.
+
+	Você começa com uma função updateRecords que recebe um objeto literal, 
+	records, contendo a coleção de álbuns musicais, um id, uma prop(like artist 
+	ou tracks) e um value. Complete a função usando as regras abaixo para 
+	modificar o objeto passado para a função.
+
+	    A função precisa sempre retornar o objeto de coleção de discos completo.
+	    Se prop não for tracks e value não for uma string vazia, atualize ou 
+	    defina aquela prop do álbum como value.
+	    Se prop for tracks mas o álbum não tiver uma propriedade tracks, crie 
+	    um array vazio e adicione o value nesse array.
+	    Se prop for tracks e value não for uma string vazia, adicione value ao 
+	    final do array existente de tracks do álbum.
+	    Se value for uma string vazia, remova a propriedade prop recebida do 
+	    álbum.
+
+	Observação: uma cópia do objeto recordCollection é usada para testes.
+*/
+
+const recordCollection = {
+	2548: {
+		albumTitle: 'Slippery When Wet',
+		artist: 'Bom Jovi',
+		tracks: ['Let it rock', 'You give love a bad name']
+	},
+
+	2468: {
+		albumTitle: '1999',
+		artist: 'Prince',
+		tracks: ['1999', 'Litte red covertte']
+	},
+
+	1245: {
+		artist: 'Robert Palmer',
+		tracks: []
+	},
+
+	5439: {
+		albumTitle: 'ABBA gold'
+	}
+};
+
+function updateRecords(records, id, prop, value) {
+	// body...
+	if (prop !== 'tracks' && value) {
+		records[id][prop] = value;
+	} else if (prop === 'tracks' && records[id].hasOwnProperty('tracks') === false) {
+		records[id][prop] = [value]
+	} else if (prop === 'tracks' && value !== '') {
+		records[id][prop].push(value);
+	} else if (value === '') {
+		delete records[id][prop];
+	}
+
+	return records;
+}
+
+const result1 = updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+console.log(result1);
