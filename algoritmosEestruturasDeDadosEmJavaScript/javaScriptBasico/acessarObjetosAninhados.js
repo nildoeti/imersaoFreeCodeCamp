d20b07ea256bb84a0bc654cf395436e844e7ed5a
Nildo <nildo@webstack.com.br>
2021-12-31 10:55:45 -0300

    Finalizado desafio do projeto.

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/acessarObjetosAninhados.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/acessarObjetosAninhados.js
@@ -48,3 +48,25 @@ const result5 = ourStorage.cabinet['top drawer'].folder2; // secrets
 const result6 = ourStorage.cabinet['bottom drawer']; // soda
 
 console.log(result1, result2, result3, result4, result5, result6);
+
+/*
+    Desafio.
+    Acesse o objeto myStorage e atribua o conteúdo da propriedade glove box para 
+    a variável gloveBoxContents. Use notação de ponto para todas as propriedades 
+    sempre que possível, caso contrário, use a notação de colchetes.
+*/
+
+const myStorage = {
+    'car': {
+        'inside': {
+            'glove box': 'maps',
+            'passenger seat': 'crubs' 
+        },
+        'outside': {
+            'trunk': 'jack'
+        }
+    }
+};
+
+const gloveBoxContents = myStorage.car.inside['glove box'];
+console.log(gloveBoxContents); // maps
