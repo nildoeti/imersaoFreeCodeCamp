701a756840198ddf7681fc29975f66a27c7d1d1f
Nildo <nildo@webstack.com.br>
2022-01-01 18:47:54 -0300

    Adicionado segundo exemplo para o desafio

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/pesquisarPerfil.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/pesquisarPerfil.js
@@ -59,3 +59,18 @@ function lookUpProfile(name, prop) {
 	}
 	return 'No such contact';
 }
+
+
+// exemplo 2
+function lookUpProfile(name, prop) {
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      if (prop in contacts[i]) {
+        return contacts[i][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
