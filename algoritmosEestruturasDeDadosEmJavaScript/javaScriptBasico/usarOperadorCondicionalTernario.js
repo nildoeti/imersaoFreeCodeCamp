b9a52f1944cca664b08400e99e0d8cdcb7ff7b1e
Nildo <nildo@webstack.com.br>
2022-01-01 23:50:57 -0300

    Finalizado desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/usarOperadorCondicionalTernario.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/usarOperadorCondicionalTernario.js
@@ -38,3 +38,14 @@ function findGreater(a, b) {
 function findGreater(a, b) {
     return a > b? 'a is greater' : 'b is greater or equal';
 }
+
+/*
+    Use o operador condicional na função checkEqual para verificar se dois 
+    números são iguais ou não. A função deve retornar ou a string Equal ou a 
+    string Not Equal.
+*/
+
+function checkEqual(a, b) {
+    return a === b? 'Equal' : 'Not equal';
+}
+checkEqual(1, 2)
