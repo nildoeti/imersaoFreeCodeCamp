b7394d94769277433c77e78ea3bdef744222fa5b
Nildo <nildo@webstack.com.br>
2022-01-02 00:24:46 -0300

    Finalizado desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/usarOperadoresDeMmultiplasCondicoesTernarios.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/usarOperadoresDeMmultiplasCondicoesTernarios.js
@@ -57,3 +57,14 @@ function findGreaterOrEqual2(a, b) {
 
 }
 
+/*
+    Na função checkSign, use operadores de múltiplas condições - seguindo o 
+    formato recomendado usado em findGreaterOrEqual - para verificar se um 
+    número é positivo, negativo ou zero. A função deve retornar positive, 
+    negative ou zero.
+*/
+
+function checkSign(num) {
+    return (num > 0)? 'positive' : (num < 0)? 'negative' : 'zero';
+}
+checkSign(10)
