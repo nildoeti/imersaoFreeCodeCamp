1749eb32d1baba4b913a75393aca3b3ca9c34ee7
Nildo <nildo@webstack.com.br>
2022-01-01 23:20:07 -0300

    Finalizado desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/usarFuncaoParseInt.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/usarFuncaoParseInt.js
@@ -17,3 +17,13 @@ console.log("'007' => ", typeof(a));
 const b = parseInt(a);
 console.log('007 =>', typeof(b));
 
+/*
+    Use parseInt() na função convertToInteger para que ela converta a string de 
+    entrada str em um inteiro e a retorne.
+*/
+
+function convertToInteger(str) {
+    return parseInt(str);
+}
+const str = convertToInteger('56');
+console.log(str);
