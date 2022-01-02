6721ae13655da1050791347a972a77e89fae3e10
Nildo <nildo@webstack.com.br>
2022-01-01 21:51:57 -0300

    Desafio para números aleatórios finalizado

--- algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/gerarNumerosInteirosAleatoriosDentroDeUmIntervalo.js
+++ algoritmosEestruturasDeDadosEmJavaScript/javaScriptBasico/gerarNumerosInteirosAleatoriosDentroDeUmIntervalo.js
@@ -27,3 +27,14 @@ function minMax(min, max){
 const result2 = minMax(4, 7);
 console.log(result2);
 
+/**
+ * Crie uma função chamada randomRange que recebe um intervalo de myMin e 
+ * myMax e retorne um número inteiro aleatório que é maior ou igual a myMin, e 
+ * é menor ou igual a myMax.
+ */
+
+function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - minMax + 1) + myMin); 
+}
+
+// console.log(randomRange());
