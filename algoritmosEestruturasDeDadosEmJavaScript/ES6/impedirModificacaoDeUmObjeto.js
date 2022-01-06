2f400ba45cf3b74fd4f44ebfc5b025e6355b75f4
Nildo <nildo@webstack.com.br>
2022-01-06 12:10:30 -0300

    Adicioando modulos para node ao projeto

--- algoritmosEestruturasDeDadosEmJavaScript/ES6/impedirModificacaoDeUmObjeto.js
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/impedirModificacaoDeUmObjeto.js
@@ -22,17 +22,44 @@
 */
 
 // exemplo 1
-const obj = {
-	name: 'FreeCodeCamp',
-	review: 'Awesome'
-};
+// const obj = {
+// 	name: 'FreeCodeCamp',
+// 	review: 'Awesome'
+// };
 
-Object.freeze(obj); // protegendo nosso objeto
-obj.review = 'bad';
-obj.newProp = 'Test';
-console.log(obj); // { name: 'FreeCodeCamp', review: 'Awesome' }
+// Object.freeze(obj); // protegendo nosso objeto
+// obj.review = 'bad';
+// obj.newProp = 'Test';
+// console.log(obj); // { name: 'FreeCodeCamp', review: 'Awesome' }
 
 /*
 	Objeto obj, protgido.
 	Portanto a alteração em obj.newProp = 'Text', não aconteceu.
 */
+
+
+/*
+	Desafio
+	Nesse desafio, você usará o método Object.freeze para prevenir a mudança de 
+	constantes matemáticas. Você precisa congelar o objeto MATH_CONSTANTS para 
+	que ninguém possa alterar o valor de PI, adicionar ou deletar propriedades.
+*/
+
+function freezeObj() {
+	const MATH_CONSTANTS = {
+		PI: 3.14
+	};
+	Object.freeze(MATH_CONSTANTS);
+
+	try {
+		MATH_CONSTANTS.PI = 99; // alterando o valor par PI
+	} catch(ex) {
+		console.log(ex);
+	}
+	return MATH_CONSTANTS.PI; // aqui deveria retornar 99
+}
+
+const PI = freezeObj(); // protegendo PI de sofrer alterações
+console.log(PI); // 3.14
+
+
