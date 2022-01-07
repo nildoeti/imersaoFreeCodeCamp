da2b30ff47bfc03439a8f361d1044b8157ef7fe7
Nildo <nildo@webstack.com.br>
2022-01-07 17:51:36 -0300

    Finalizado desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/ES6/escreverArrowFunctionsComParametros.js
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/escreverArrowFunctionsComParametros.js
@@ -29,8 +29,24 @@
 // console.log(p1); // 8
 
 // exemplo 2
-// os parênteses envolvendo arrow function tiver com um único parâmetro,  
+// os parênteses envolvendo arrow function com um único parâmetro,  
 // podem ser omitidos.
-const doubler = item => item * 4;
-const result = doubler(5);
-console.log(result); // 20
+// const doubler = item => item * 4;
+// const result = doubler(5);
+// console.log(result); // 20
+
+/*
+	Reescreva a função myConcat que anexa conteúdo de arr2 para arr1 para que a 
+	função use sintaxe de arrow function.
+*/
+
+// var myConcat = function (arr1, arr2) {
+// 	return arr1.concat(arr2); 
+// }
+// console.log(myConcat([1,2], [3, 4, 5]));
+
+var myConcat = (arr1, arr2) => {
+	return arr1.concat(arr2);
+}
+
+console.log(myConcat([1, 2], [3, 4, 5])); // [ 1, 2, 3, 4, 5 ]
