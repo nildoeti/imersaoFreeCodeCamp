7b6cc98dedc7d4b3ca823ec00e50ccc20e2eef7e
Nildo <nildo@webstack.com.br>
2022-01-15 06:39:30 -0500

    Gerado nova atualização

--- algoritmosEestruturasDeDadosEmJavaScript/ES6/usarAtribuicaoDeDesestruturacaoParaAtribuirVariaveisDeObjetosAninhados.js
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/usarAtribuicaoDeDesestruturacaoParaAtribuirVariaveisDeObjetosAninhados.js
@@ -26,50 +26,25 @@
 
 */
 
-// bjetos aninhados
-const user = { // object one
-	johnDoe: { // object two
+// objetos aninhados
+const user = {// first object
+	johnDoe: { // second object
 		age: 34,
 		email: 'johnDoe@freeCodeCamp.com'
+	},
+	paulYoung: {
+		age: 48,
+		email: 'PaulYoung@freeCodeCamp.com'
 	}
 };
 
-// extrair valores de propriedades de objetos
+// acessando dados dos objetos aninhados
+console.log(user.johnDoe.age); // 34
+console.log(user.johnDoe.email); // johnDoe@freeCodeCamp.com
+console.log(user.paulYoung.age); // 48
+console.log(user.paulYoung.email); // PaulYoung@freeCodeCamp.com
+
+// extrair valores de propriedades de objetos e atribuí-los variáveis com o mesmo nome
 const {johnDoe: {age, email}} = user;
-
-/* 	atribuir o valor de uma propriedade de um objeto 
- 	para variáveis com nomes diferentes 
- /*
-
- const {johnDoe: {age: userAge, email: userEmail}} = user;
-
-
- /*
- 	Substitua as duas atribuições com uma atribuição de desestruturação 
- 	equivalente. Ainda deve ser atribuído às variáveis lowToday e highToday os 
- 	valores de today.low e today.high do objeto LOCAL_FOR
- */
-
- const LOCAL_FORECAST = {
- 	yesterday: {low: 61, high: 75},
- 	today: {low: 64, high: 77},
- 	tomorrow: {low: 68, high: 80}
- };
-// Substitua as duas atribuições com uma atribuição de desestruturação 
-const lowYesterday = LOCAL_FORECAST.yesterday.low; // 61
-const highyesterday = LOCAL_FORECAST.yesterday.high; // 75
-
-console.log(lowYesterday);
-console.log(highyesterday);
-
-const lowToday = LOCAL_FORECAST.today.low;
-const highToday = LOCAL_FORECAST.today.high;
-
-console.log(lowToday); // 64
-console.log(highToday); // 77
-
-const lowTomorrow = LOCAL_FORECAST.tomorrow.low;
-const highTomorrow = LOCAL_FORECAST.tomorrow.high;
-
-console.log(lowTomorrow); // 68
-console.log(highTomorrow); // 80
+console.log(user.johnDoe.age); // 34
+console.log(user.johnDoe.email); // johnDoe@freeCodeCamp.com
