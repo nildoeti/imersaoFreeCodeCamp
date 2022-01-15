20a4e55814344fd7b3ea6448fd617ff4ab3e1b1d
Nildo <nildo@webstack.com.br>
2022-01-15 16:16:39 -0500

    Desafio do projeto finalizado

--- algoritmosEestruturasDeDadosEmJavaScript/ES6/usarAtribuicaoDeDesestruturacaoParaAtribuirVariaveisaPartirDeArrays.js
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/usarAtribuicaoDeDesestruturacaoParaAtribuirVariaveisaPartirDeArrays.js
@@ -26,13 +26,28 @@
 */
 
 // Desestruturar um array nos permite fazer exatamente isso:
-const [a, b] = [1, 2, 3, 4, 5, 6];
-console.log(a, b);
+// const [a, b] = [1, 2, 3, 4, 5, 6];
+// console.log(a, b);
 
-// vírgulas para alcançar o índice desejado
-const [x, y,,, z] = [1, 2, 3, 4, 5, 6,]; // 
-console.log(x, y, z)
+// // vírgulas para alcançar o índice desejado
+// const [x, y,,, z] = [1, 2, 3, 4, 5, 6,]; // 
+// console.log(x, y, z)
 
-// novo exemplo
-const [m, n,,,,, p] = [11, 22, 33, 44, 55, 66, 77];
-console.log(m, n, p); // 
+// // novo exemplo
+// const [m, n,,,,, p] = [11, 22, 33, 44, 55, 66, 77];
+// console.log(m, n, p); // 11 22 77
+
+
+/*
+	Use atribuição de desestruturação para trocar os valores de a e b para que 
+	a receba os valores armazenados em b e b recebe os valores armazenados em a.
+*/
+
+let a = 8, b = 6;
+// antes
+console.log('antes: ', a, b);
+
+// depois
+[a, b] = [b, a]
+console.log('Depois: ', a, b);
+
