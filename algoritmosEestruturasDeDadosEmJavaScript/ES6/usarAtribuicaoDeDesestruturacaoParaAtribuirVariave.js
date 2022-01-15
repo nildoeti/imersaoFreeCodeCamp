dc03affee12a1362527bc00ae3537ceac75c6caa
Nildo <nildo@webstack.com.br>
2022-01-15 01:02:18 -0500

    Revisão adicionado ao projeto.

--- algoritmosEestruturasDeDadosEmJavaScript/ES6/usarAtribuicaoDeDesestruturacaoParaAtribuirVariave.js
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/usarAtribuicaoDeDesestruturacaoParaAtribuirVariave.js
@@ -19,8 +19,33 @@
 
 */
 
-// atribuição de desestruturação
+// declaração e extração dos dados na forma usual
 const user = {name: 'John Doe', age: 34};
-const {name: userName, age: userAge} = user;
+console.log(user.name, user.age); // John Doe 34
 
-console.log(userName, userAge);
+// atribuição de desestruturação
+const {name: userName, age: userAge} = user;
+console.log(userName, userAge); // John Doe 34
+
+/*
+	Substitua as duas atribuições com uma atribuição de desestruturação para HIGH_TEMPERATUR, highToday, highTomorrowES
+	equivalente. Ainda deve ser atribuído às variáveis highToday e highTomorrow 
+	os valores de today e tomorrow do objeto HIGH_TEMPERATURES.
+*/
+
+const HIGH_TEMPERATUR = {
+	yesterday: 75,
+	today: 80
+};
+
+// // acessando os objetos e suas propriedades no modo normal
+console.log(HIGH_TEMPERATUR.yesterday); // 75
+console.log(HIGH_TEMPERATUR.today); // 80
+
+// atribuição de desestruturação para HIGH_TEMPERATURES
+const {
+	yesterday: highYesterday,
+	today: highToday
+} = HIGH_TEMPERATUR;
+console.log('\n...\n');
+console.log(highYesterday, highToday); // 75, 80
