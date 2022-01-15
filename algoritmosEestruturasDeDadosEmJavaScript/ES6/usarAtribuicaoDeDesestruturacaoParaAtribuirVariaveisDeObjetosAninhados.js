49d9a013f8f81825908378a2dbed4adcceb71330
Nildo <nildo@webstack.com.br>
2022-01-15 07:00:22 -0500

    Adicionado desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/ES6/usarAtribuicaoDeDesestruturacaoParaAtribuirVariaveisDeObjetosAninhados.js
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/usarAtribuicaoDeDesestruturacaoParaAtribuirVariaveisDeObjetosAninhados.js
@@ -48,3 +48,21 @@ console.log(user.paulYoung.email); // PaulYoung@freeCodeCamp.com
 const {johnDoe: {age, email}} = user;
 console.log(user.johnDoe.age); // 34
 console.log(user.johnDoe.email); // johnDoe@freeCodeCamp.com
+
+
+/*
+	Substitua as duas atribuições com uma atribuição de desestruturação 
+	equivalente. Ainda deve ser atribuído às variáveis lowToday e highToday os 
+	valores de today.low e today.high do objeto LOCAL_FORECAST.
+*/
+
+const LOCAL_FORECAST = {
+	yesterday: {low: 61, high: 75},
+	today: {low: 54, high: 77},
+	tomorrow: {low: 68, high: 80}
+};
+
+// problema para resolver
+const lowToday = LOCAL_FORECAST.today.low;
+const highToday = LOCAL_FORECAST.today.high;
+console.log(lowToday, highToday); 54, 77
