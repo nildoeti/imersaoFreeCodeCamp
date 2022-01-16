133cd11af3136d981fca04f0277fcf4011b04be3
Nildo <nildo@webstack.com.br>
2022-01-16 07:37:14 -0500

    Desafio do projeto finalizado

--- algoritmosEestruturasDeDadosEmJavaScript/ES6/usarAtribuicaoDeDesestruturacaoParaPassarUmObjetoComoParametroDeUmaFuncao.js
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/usarAtribuicaoDeDesestruturacaoParaPassarUmObjetoComoParametroDeUmaFuncao.js
@@ -37,3 +37,19 @@
 const profileUpdate = ( {name, age, nationaly, location} ) => {}
 
 
+/*
+	Use atribuição de desestruturação dentro do argumento para a função half
+	para enviar apenas max e min para dentro da função.
+*/
+
+const stats = {
+	max: 56.78,
+	standard_deviation: 34,
+	median: 34.54,
+	mode: 23.87,
+	min: -0.75,
+	average: 35.85
+};
+
+// const half = (stats) => (stats.max + stats.min) / 2;
+const half = ( {max, minn} ) => (max + min) / 2.0;
