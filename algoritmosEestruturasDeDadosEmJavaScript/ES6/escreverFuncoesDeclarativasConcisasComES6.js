a796ca29d6321c8e40d92ca8bca3c9b5aab6149e
Nildo <nildo@webstack.com.br>
2022-01-16 18:54:52 -0500

    escreverFuncoesDeclarativasConcisasComES6 foi atualizado

--- algoritmosEestruturasDeDadosEmJavaScript/ES6/escreverFuncoesDeclarativasConcisasComES6.js
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/escreverFuncoesDeclarativasConcisasComES6.js
@@ -38,3 +38,22 @@ const person1 = {
 		return 'Hello! My name is ${this.name}';
 	}
 };
+
+/*
+	Refatore a função setGear dentro do objeto bicycle para usar a sintaxe
+	curta descrita acima.
+*/
+
+const bicycle = {
+	gear: 2,
+	// setGear: function(newGear) {
+	// 	this.gear = newGear;
+	// }
+	setGear(newGear) {
+		this.gear = newGear; 
+	}
+};
+
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
