8c03a9305df666072c8591ca5b098beac1d3e0d6
Nildo <nildo.webstack.com.br>
2022-01-19 21:32:58 -0500

    adicionado exemplo do projeto

---
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/separarSeusScriptsEmModulos.js
@@ -0,0 +1,23 @@
+/*
+	Separar seus scripts em módulos
+
+	O JavaScript nasceu com com o objetivo de cumprir um pequeno papel em uma
+	web onde tudo era, na maior parte, HTML. Hoje, o JavaScript é gigante. Para
+	se ter noção, alguns websites são construídos quase que inteiramente em
+	JavaScript. A fim de tornar o JavaScript mais modular, limpo e passível de
+	manutenção, a versão ES6 introduziu uma forma mais simples de compartilhar
+	códigos entre arquivos JavaScript. Dessa forma, você consegue exportar
+	partes de um arquivo e usá-los em arquivos externos bem como importar as
+	partes de que você precisa. Para tirar proveito dessa funcionalidade, você
+	precisa criar uma tag script com o atributo type de valor module no seu
+	documento HTML. Exemplo:
+
+	<script type="module" src="filename.js"></script>
+
+	O script do exemplo acima agora é um módulo (module) e pode usar os
+	recursos import e export (você aprenderá sobre eles nos próximos desafios).
+
+*/
+
+// exemplo para module
+// <script type="module" src="filename.js"></script>
