807459ff70a749c95879dec0016b12fe6d8db842
Nildo <nildo.webstack.com.br>
2022-01-19 21:25:35 -0500

    Finalizado desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/ES6/usarGettersSetterParaControlarAcessoEmUmObjeto.js
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/usarGettersSetterParaControlarAcessoEmUmObjeto.js
@@ -87,3 +87,38 @@ console.log(novel.write); // anonymous
 novel.write = 'newAuthor';
 console.log(novel.write); // newAuthor
 
+/*
+	Use a palavra-chave class para criar a classe Thermostat. O constructor
+	aceita uma temperatura Fahrenheit.
+
+	Na classe, crie um getter para obter a temperatura em Celsius e um setter
+	para definir a temperatura em Celsius.
+
+	Lembre-se de que C = 5/9 * (F - 32) e F = C * 9.0 / 5 + 32, aonde F é o
+	valor da temperatura em Fahrenheit e C é o valor da mesma temperatura em Celsius.
+
+	Observação: quando você implementa isso, você vai rastrear a temperatura
+	dentro da classe em uma escala, ou Fahrenheit ou Celsius.
+
+	Esse é o poder de um getter e um setter. Você está criando uma API para
+	outro uso, que pode pegar o resultado correto independente de qual está
+	rastreando.
+
+	Em outras palavras, você está abstraindo detalhes de implementação do
+	usuário.
+
+*/
+
+class Thermostat {
+  constructor(fahrenheit) {
+    this.fahrenheit = fahrenheit;
+  }
+  
+  get temperature() {
+    return (5 / 9) * (this.fahrenheit - 32);
+  }
+  
+  set temperature(celsius) {
+    this.fahrenheit = (celsius * 9.0) / 5 + 32;
+  }
+}
