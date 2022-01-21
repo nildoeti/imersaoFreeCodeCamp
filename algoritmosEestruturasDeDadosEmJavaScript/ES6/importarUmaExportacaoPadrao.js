b5b97d2348514f26610f46c9be3f7b7c0a65033c
Nildo <nildo@webstack.com.br>
2022-01-21 15:56:03 -0500

    exemplos do projeto foi finalizado

---
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/importarUmaExportacaoPadrao.js
@@ -0,0 +1,22 @@
+/*
+	Importar uma exportação padrão
+
+	No último desafio, você aprendeu sobre export default e seus usos. Para
+	importar uma exportação padrão, você precisa usar uma sintaxe diferente de
+	import. No exemplo a seguir, add é a exportação padrão do arquivo
+	math_functions.js. Veja como importá-la:
+
+	import add from "./math_functions.js";
+
+	A sintaxe é diferente em apenas um ponto. O valor importado, add, não está
+	rodeado por chaves ({}). Aqui, add é simplesmente uma palavra qualquer que
+	vai ser usada para identificar a variável sendo exportada do arquivo
+	math_functions.js. Você pode usar qualquer nome ao importar algo que foi
+	exportado como padrão.
+*/
+
+// exemplo de import de uma exportação padrão
+import add from  "./math_functions.js";
+
+
+
