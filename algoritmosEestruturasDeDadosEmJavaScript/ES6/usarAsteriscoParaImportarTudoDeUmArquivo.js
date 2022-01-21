6a7ed965bef76952786bff3c333c2ff2c704e952
Nildo <nildo@webstack.com.br>
2022-01-21 09:40:55 -0500

    finalizado exemplos para o projeto

---
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/usarAsteriscoParaImportarTudoDeUmArquivo.js
@@ -0,0 +1,27 @@
+/*
+	Usar * para importar tudo de um arquivo
+
+	Suponha que você tem um arquivo e deseja importar todo o conteúdo dele no
+	arquivo atual. Isso pode ser feito com a sintaxe import * as. Aqui está um
+	exemplo onde todo o conteúdo de um arquivo chamado math_function.js é
+	importado em um arquivo no mesmo diretório:
+
+	import * as myMathModule from "./math_functions.js";
+
+	A instrução import acima criará um objeto chamado myMathModule. Esse nome é
+	totalmente arbitrário. Você pode escolher qualquer outro nome que seja
+	apropriado para sua aplicação. O objeto conterá todas as exportações do
+	arquivo math_functions.js. Dessa forma, você pode acessar as funções
+	exportadas da mesma forma que você acessa as propriedades de um objeto.
+	Aqui está um exemplo de como você pode usar as funções add e subtract que
+	foram importadas:
+
+	myMathModule.add(2,3);
+	myMathModule.subtract(5,3);
+
+*/
+
+// exemplo de como fazer a exportacao de todos os mdulos de um arquivo
+import * as myMathModule from "./usarExportacaoParaCompartilharUmBlocoDeCodigo.js";
+
+
