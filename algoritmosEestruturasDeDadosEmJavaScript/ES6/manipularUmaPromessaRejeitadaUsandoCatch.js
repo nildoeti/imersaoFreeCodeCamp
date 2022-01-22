ba65f85e6fd35755c3a719a5ce74f85ccc74b71b
Nildo <nildo@webstack.com.br>
2022-01-22 11:17:06 -0500

    adicionado exemplo de uso de catch para manipulação de erros

---
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/manipularUmaPromessaRejeitadaUsandoCatch.js
@@ -0,0 +1,16 @@
+/*
+	Manipular uma promessa rejeitada usando o catch
+
+	catch é o método usado quando a promessa é rejeitada. Ele é executado
+	imediatamente após o método reject da promessa ser chamado. Aqui está a
+	sintaxe:
+
+	myPromise.catch(error => {
+
+	});
+
+	O parâmetro error é o argumento passado para o método reject.
+*/
+
+// use de catch para manipulao de erros
+myPromise.catch(error => {});
