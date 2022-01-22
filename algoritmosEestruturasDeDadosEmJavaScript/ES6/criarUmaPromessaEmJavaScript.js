de1772fddfcf0e13ab4450f262d117c3f55acccb
Nildo <nildo@webstack.com.br>
2022-01-21 21:14:41 -0500

    adicioando dois exemplos para promises em javascript

---
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/criarUmaPromessaEmJavaScript.js
@@ -0,0 +1,22 @@
+/*
+	Criar uma promessa em JavaScript
+
+	Uma promessa em JavaScript é exatamente o que parece - você faz a promessa
+	de que vai fazer uma tarefa, geralmente de forma assíncrona. Quando a
+	tarefa é finalizada, ou você cumpriu a promessa ou falhou ao tentar. Por
+	ser uma função construtora, você precisa utilizar a palavra-chave new para
+	criar uma Promise. Ela recebe uma função, como seu argumento, com dois
+	parâmetros - resolve e reject. Esses métodos são usados para determinar o
+	resultado da promessa. A sintaxe se assemelha a:
+
+	const myPromise = new Promise((resolve, reject) => {
+
+	});
+
+*/
+
+// promessa em JavaScript
+const myPromise = new Promise( (resolve, reject) => {});
+
+// exemplo 2
+const minhaPromessa = new Promise( (resolve, reject) => {});
