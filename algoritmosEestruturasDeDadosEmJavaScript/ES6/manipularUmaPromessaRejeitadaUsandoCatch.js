4e400218725e20653dfd94e75074627b2c3b52c0
Nildo <nildo@webstack.com.br>
2022-01-22 11:27:04 -0500

    finalizado desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/ES6/manipularUmaPromessaRejeitadaUsandoCatch.js
+++ algoritmosEestruturasDeDadosEmJavaScript/ES6/manipularUmaPromessaRejeitadaUsandoCatch.js
@@ -13,4 +13,23 @@
 */
 
 // use de catch para manipulao de erros
-myPromise.catch(error => {});
+// myPromise.catch(error => {});
+
+
+/*
+	Adicione o método catch à sua promessa. Use error como parâmetro de sua
+	função de callback e exiba o valor de error no console.
+*/
+
+const makeServerRequest = new Promise( (resolve, reject) => {
+	let responseFromServer = true;
+
+	if (responseFromServer) {
+		resolve('We got the data');
+	} else {
+		reject('Data not received');
+	}
+});
+
+makeServerRequest.then(result => {console.log();});
+// makeServerRequest.catch(error => {console.log(error);});
