a384024621412ba84b552f52eae84e4ee3249102
Nildo <nildo@webstack.com.br>
2022-01-23 07:38:31 -0500

    exemplos para método test() adicionado ao projeto

---
+++ algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/usarMetodoTest.js
@@ -0,0 +1,31 @@
+/**
+	Usar o método test
+
+	Expressões regulares são usadas em linguagens de programação para encontrar
+	e extrair partes de strings. Cria-se padrões que ajudam a encontrar tais
+	partes.
+
+	Se você quiser encontrar a palavra the na string The dog chased the cat,
+	você poderia usar a seguinte expressão regular: /the/. Note que aspas não
+	são necessárias ao redor da expressão regular.
+
+	O JavaScript oferece múltiplas maneiras de usar regexes. Uma dessas
+	maneiras é com o método .test(). O método .test() aplica a regex à string
+	dentro dos parênteses e retorna true caso encontre o padrão ou false caso
+	contrário.
+
+	let testStr = "freeCodeCamp";
+	let testRegex = /Code/;
+	testRegex.test(testStr);
+
+	O método test retorna true aqui.
+*/
+
+let testStr = 'freeCodeCamp';
+let testRegex = /Code/;
+testRegex.test(testStr);
+console.log(testRegex); // /code/
+console.log(testRegex.test(testStr)); // true
+
+
+
