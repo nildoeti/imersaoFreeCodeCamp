91426d632516768e8627e48e2bf77a4102a1853b
Nildo <nildo@webstack.com.br>
2022-01-23 17:36:05 -0500

    adicioando exemplo para extração de resultados sobre regex

--- algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/extrairResultados.js
+++ algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/extrairResultados.js
@@ -1,1 +1,33 @@
-extrairResultados.js
+/*
+	Extrair resultados
+
+	Até agora, você só tem verificado se existe ou não um padrão dentro de uma
+	string. Você também pode extrair os resultados encontrados por meio do
+	método .match().
+
+	Para usar o método .match(), aplique o método em uma string e passe a regex
+	dentro dos parênteses.
+
+	Exemplo:
+
+	"Hello, World!".match(/Hello/);
+	let ourStr = "Regular expressions";
+	let ourRegex = /expressions/;
+	ourStr.match(ourRegex);
+
+	Aqui, o primeiro match retorna ["Hello"] e, o segundo, ["expressions"].
+
+	Note que o método .match se usa de forma "contrária" ao método .test que
+	você usou até então:
+
+	'string'.match(/regex/);
+	/regex/.test('string');
+
+*/
+
+// exemplo 1
+let ourStr = 'Regular expressions';
+let ourRegex = /regular/i;
+let result = ourStr.match(ourRegex);
+console.log(result);
+
