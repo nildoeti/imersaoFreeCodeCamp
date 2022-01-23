fed2779ef54a809f091831c0707108e9ef920cd5
Nildo <nildo@webstack.com.br>
2022-01-23 15:06:28 -0500

    adicionado dois exemplos para test()

--- algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/criarCorrespondenciaDestringsLiterais.js
+++ algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/criarCorrespondenciaDestringsLiterais.js
@@ -1,1 +1,35 @@
-criarCorrespondenciaDestringsLiterais.js
+/*
+	Criar correspondência de strings literais
+
+	No desafio anterior, você usou a expressão regular /Hello/ para procurar a
+	palavra Hello. Esta regex buscou a string Hello literalmente. No exemplo
+	abaixo há outra busca literal, dessa vez pela string Kevin:
+
+	let testStr = "Hello, my name is Kevin.";
+	let testRegex = /Kevin/;
+	testRegex.test(testStr);
+
+	Essa chamada a test retornará true.
+
+	Qualquer outra forma de escrever Kevin não funcionará. Por exemplo, a regex
+	/Kevin/ não encontrará nem kevin e nem KEVIN.
+
+	let wrongRegex = /kevin/;
+	wrongRegex.test(testStr);
+
+	test retornará false.
+
+	Você verá como encontrar estas outras formas em alguns desafios futuros.
+*/
+
+// exemplo 1
+let testStr = 'Hello, my name is Kevin.';
+let testRegex = /Kevin/; // k, maiusculo
+let result = testRegex.test(testStr);
+console.log(result); // true
+
+// exemplo 2
+let wrongRegex2 = /kevin/; // k, minsculo
+let resul2 = wrongRegex2.test(testStr);
+console.log(resul2); // false
+
