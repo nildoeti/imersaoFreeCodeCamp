b64ff315441e6ba644e1f7884893758ba8c142ee
Nildo <nildo@webstack.com.br>
2022-01-25 08:51:50 -0500

    atualizado exemplo 2

--- algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/capturarNumerosLetrasDoAlfabeto.js
+++ algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/capturarNumerosLetrasDoAlfabeto.js
@@ -18,17 +18,28 @@
 
 // exemplo 1
 let jennyStr = 'Jenny8675309';
-let myRegex = /[a-z0-0]ig/;
+let myRegex = /[a-z0-0]/gi;
 let resultJenny = jennyStr.match(myRegex);
-console.log(resultJenny);
+console.log(resultJenny); // [ 'J', 'e', 'n', 'n', 'y', '0' ]
+
 
 // exemplo 2
 let user01 = 'WebStacl008';
-let user01Regex = /[a-z0-9]gi/;
+let user01Regex = /[a-z0-9]/gi;
 let user01Result = user01.match(user01Regex);
 console.log(user01Result);
+/*
+	Saida oara exemplo 2
+	[
+	  'W', 'e', 'b', 'S',
+	  't', 'a', 'c', 'l',
+	  '0', '0', '8'
+	]
+*/
+
 
 /*
+	Desafio do projeto
 	Escreva uma única regex que encontra letras entre h e s e, também, números
 	entre 2 e 6. Lembre-se de incluir as flags necessárias na regex.
 */
@@ -40,3 +51,12 @@ let quoteSample = "Blueberry 3.141592653s are delicious.";
 let myRegex2 = /[h-s2-6]/gi;
 let result = quoteSample.match(myRegex2);
 console.log(result);
+/*
+	Saida para desafio do projeto.
+	[
+	  'l', 'r', 'r', '3', '4',
+	  '5', '2', '6', '5', '3',
+	  's', 'r', 'l', 'i', 'i',
+	  'o', 's'
+	]
+*/
