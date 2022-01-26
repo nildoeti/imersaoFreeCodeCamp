e612a151abe82182598392a7a932c5a97c4f72d9
Nildo <nildo@webstack.com.br>
2022-01-26 17:32:22 -0500

    Nova atualização adicionada ao projeto.

--- algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/capturarCaracteresQueAparecemZeroOuMaisVezesSeguidas.js
+++ algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/capturarCaracteresQueAparecemZeroOuMaisVezesSeguidas.js
@@ -40,3 +40,20 @@ console.log(result2); // [ 'g', 'g' ]
 console.log(result3); // [ 'o', 'o', 'o' ]
 
 
+/*
+	Neste desafio, a string chewieQuote recebeu o valor Aaaaaaaaaaaaaaaarrrgh!
+	por trás dos panos. Escreva uma regex, chewieRegex, que usa o caractere *
+	para capturar um A maiúsculo seguido imediatamente de zero ou mais a
+	minúsculos em chewieQuote. A regex não precisa de flags ou de classes de
+	caracteres. Ela também não deve capturar nenhuma outra parte da string.
+*/
+
+// Altere apenas o código abaixo desta linha
+let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+// let chewieRegex = /change/; // Altere esta linha
+let chewieRegex = /Aa*/gi; // Linha alterada
+// Altere apenas o código acima desta linha
+
+let result4 = chewieQuote.match(chewieRegex);
+console.log(result4); // [ 'Aaaaaaaaaaaaaaaa' ]
+
