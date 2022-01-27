8e976a8d0dfe49ace12925afe30f62091f8eec72
Nildo <nildo@webstack.com.br>
2022-01-27 14:37:44 -0500

    capturarTudoExcetoLetrasNumeros.js adicionado ao projeto

---
+++ algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/capturarTudoExcetoLetrasNumeros.js
@@ -0,0 +1,29 @@
+/*
+	apturar tudo exceto letras e números
+
+	Você aprendeu que você pode usar um atalho para capturar alfanuméricos
+	[A-Za-z0-9_] usando \w. Você pode querer capturar exatamente o oposto disso.
+
+	Você pode capturar não alfanuméricos usando \W ao invés de \w. Observe que
+	o atalho usa uma maiúscula. Este atalho é o mesmo que escrever
+	[^A-Za-z0-9_].
+
+	let shortHand = /\W/;
+	let numbers = "42%";
+	let sentence = "Coding!";
+	numbers.match(shortHand);
+	sentence.match(shortHand);
+
+	A primeira chamada a match retorna ["%"] enquanto o segundo retorna ["!"].
+
+*/
+
+// exemplo 1
+let shortHand = /\W/g;
+let numbers = '42%';
+let numbersTwo = '42';
+let sentence = 'Coding!';
+
+console.log(numbers.match(shortHand)); // [ '%' ]
+console.log(numbersTwo.match(shortHand)); // null
+console.log(sentence.match(shortHand)); // [ '!' ]
