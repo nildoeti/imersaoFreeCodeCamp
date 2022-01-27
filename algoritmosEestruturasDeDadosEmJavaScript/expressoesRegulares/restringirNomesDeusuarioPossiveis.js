4897b4cf47ebce26af924414e35ed77dd90e496e
Nildo <nildo@webstack.com.br>
2022-01-27 15:47:16 -0500

    adicionado solução 2 para o desafio do projeto

---
+++ algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/restringirNomesDeusuarioPossiveis.js
@@ -0,0 +1,47 @@
+/*
+	Restringir nomes de usuário possíveis
+
+	Nomes de usuário (usernames) são usados em toda a Internet. São o que fazem
+	com que tenham uma identidade única em seus sites favoritos.
+
+	Você precisa verificar todos os usernames em um banco de dados. Existem
+	algumas regras que os usuários precisam seguir quando criam os seus
+	usernames.
+
+	    Nomes de usuário só podem conter caracteres alfanuméricos.
+
+	    Os números, se algum, precisam estar no fim da string. Pode haver zero
+	    ou mais números. Usernames não podem começar com números.
+
+	    As letras podem ser maiúsculas ou minúsculas.
+
+	    O tamanho de nomes de usuários precisa ser pelo menos dois. Um username
+	    de dois caracteres só pode conter letras.
+
+
+		Modifique a regex userCheck para que inclua as regras listadas.
+
+*/
+
+// soluo 1
+let username = "JackOfAllTrades";
+// let userCheck = /change/; // Altere esta linha
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i;
+let result = userCheck.test(username);
+console.log(result);
+
+/*
+	Explicação do código
+
+	    ^ - incio da entrada
+	    [a-z] - o primeiro caractere  uma letra
+	    [a-z]+ - os seguintes caracteres são letras
+	    \d*$ - entrada termina com 0 ou mais dígitos | - ou
+	    ^[a-z] - o primeiro caractere é uma letra
+	    \d\d+ - os seguintes caracteres são 2 ou mais dígitos
+	    $ - end if input
+
+*/
+
+
+
