8642e6cd67699a85becd7eeeec27939224ea1203
Nildo <nildo@webstack.com.br>
2022-01-27 08:22:10 -0500

    finalizado exemplo 1 do projeto

---
+++ algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/encontrarUmOuMaisCriminososEmUmaCacada.js
@@ -0,0 +1,33 @@
+/*
+	Encontrar um ou mais criminosos em uma caçada
+
+	Hora de testar as suas novas habilidades de regex. Um grupo de criminosos
+	escapou da prisão, mas não sabemos quantos. No entanto, sabemos que eles
+	ficam juntos quando estão no meio de outras pessoas. E você ficou
+	responsável por encontrar todos eles.
+
+	Vamos revisitar maneiras de executar essa tarefa:
+
+	A regex /z+/ encontra a letra z quando ela aparece uma ou mais vezes
+	seguidas. Ela encontra resultados em todas essas strings:
+
+	"z"
+	"zzzzzz"
+	"ABCzzzz"
+	"zzzzABC"
+	"abczzzzzzzzzzzzzzzzzzzzzabc"
+
+	Mas ela não encontra nada nessas strings, que não possuem z em lugar nenhum:
+
+	""
+	"ABC"
+	"abcabc"
+
+*/
+
+// exemplo 1
+let crim = 	"abczZzzzzzzzzzzzzzzzzzzzzabc";
+let crimRegex = /Z+/gi; // retorna todos os com letra: zZ
+let result = crim.match(crimRegex);
+console.log(result);
+
