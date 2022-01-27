7f5bd803e7b251314b732e0b945ad9695493e3a2
Nildo <nildo@webstack.com.br>
2022-01-27 14:59:53 -0500

    finalizado exemplos do projeto

---
+++ algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/capturarTodosOsNumeros.js
@@ -0,0 +1,17 @@
+/*
+	Capturar todos os números
+
+	Você aprendeu atalhos para padrões comuns de strings como alfanuméricos.
+	Outro padrão comum é o de apenas dígitos ou números.
+
+	O atalho para procurar caracteres numéricos é \d, com um d minúsculo. Esse
+	atalho é o mesmo que [0-9], que serve para procurar qualquer dígito de zero
+	a nove.
+*/
+
+let myString = 'ano 2001';
+let myRegex = /\d/g;
+
+console.log(myRegex.test(myString)); // true
+console.log(myString.match(myRegex)); // [ '2', '0', '0', '1' ]
+
