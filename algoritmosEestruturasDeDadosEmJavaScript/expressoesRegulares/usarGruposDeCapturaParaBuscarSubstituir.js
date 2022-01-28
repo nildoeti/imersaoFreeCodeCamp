353d382bc9054d7f378c2eca20d2afdc9ae62323
Nildo <nildo@webstack.com.br>
2022-01-28 15:33:23 -0500

    adicionado e resolvido o desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/usarGruposDeCapturaParaBuscarSubstituir.js
+++ algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/usarGruposDeCapturaParaBuscarSubstituir.js
@@ -32,3 +32,22 @@ console.log('Depois: ', wrongText.replace(silverRegex, 'blue')); // The sky is b
 
 // exemplo 2
 console.log("Code Camp".replace(/(\w+)\s(\w+)/, '$2 $1')); // Camp Code
+
+/*
+	Desafio
+	Escreva uma regex, fixRegex, que usa três grupos de captura para procurar
+	cada palavra na string one two three. Depois atualize a variável replaceText
+	para trocar de one two three para three two one e atribuir o resultado à
+	variável result. Certifique-se de estar utilizando grupos de captura na
+	string de substituição usando o cifrão ($).
+*/
+
+let str = "one two three";
+// let fixRegex = /change/; // Altere esta linha
+// let replaceText = ""; // Altere esta linha
+
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/;
+let replaceText = "$3 $2 $1";
+let result = str.replace(fixRegex, replaceText);
+console.log(result); // three two one
+
