9da3ff92c1739bcd7aef58ddf1b0ef873ba0a092
Nildo <nildo@webstack.com.br>
2022-01-28 07:24:49 -0500

    adicionado e resolvido desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/validarGruposMistosDeCaracteres.js
+++ algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/validarGruposMistosDeCaracteres.js
@@ -21,3 +21,21 @@
 let testStr = 'Pumpkin';
 let testRegex = /P(engu|umpk)in/;
 console.log(testRegex.test(testStr)); // true
+
+
+/*
+	Desafio
+	Corrija a regex para que ela valide os nomes Franklin Roosevelt e Eleanor
+	Roosevelt levando em conta maiúsculas e minúsculas. A regex também deve
+	permitir nomes do meio.
+
+	Depois corrija o código, fazendo com que a regex seja testada na
+	string myString, retornando true ou false.
+*/
+
+let myString = "Eleanor Roosevelt";
+// let myRegex = /False/; // Altere esta linha
+// let result = false; // Altere esta linha
+let myRegex = /(Franklin|Eleanor).*Roosevelt/;
+let result = myRegex.test(myString);
+console.log(result); // true
