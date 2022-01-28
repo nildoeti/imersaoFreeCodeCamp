f70a2e97be5e61f5781151b149f948b4f9e5163f
Nildo <nildo@webstack.com.br>
2022-01-28 06:29:50 -0500

    especificarNumeroExatoDeCapturas.js adicionado ao projeto

--- algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/especificarNumeroExatoDeCapturas.js
+++ algoritmosEestruturasDeDadosEmJavaScript/expressoesRegulares/especificarNumeroExatoDeCapturas.js
@@ -1,1 +1,33 @@
-especificarNumeroExatoDeCapturas.js
+/*
+	Especificar o número exato de capturas
+
+	Você pode especificar um número mínimo e um máximo de capturas com chaves.
+	Às vezes, você só quer um número específico de capturas.
+
+	Para especificar este número, apenas escreva-o dentro das chaves.
+
+	Por exemplo, você pode escrever a regex /ha{3}h/ para capturar a letra a 3
+	vezes na string hah.
+
+	let A4 = "haaaah";
+	let A3 = "haaah";
+	let A100 = "h" + "a".repeat(100) + "h";
+	let multipleHA = /ha{3}h/;
+	multipleHA.test(A4);
+	multipleHA.test(A3);
+	multipleHA.test(A100);
+
+	As três chamadas a test acima retornam, na ordem, os valores: false, true e
+	false.
+
+*/
+
+// exemplo 1
+let A4 = 'haaaah';
+let A3 = 'haaah';
+let A100 = 'h' + 'a'.repeat(100) + 'h';
+let multipleHA = /ha{3}h/;
+console.log(multipleHA.test(A4)); // false
+console.log(multipleHA.test(A3)); // true
+console.log(multipleHA.test(A100)); // false
+
