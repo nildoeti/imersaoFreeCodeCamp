c3c46b916294742c4088edb0d2698b50b022cb62
Nildo <nildo@webstack.com.br>
2022-01-29 17:13:25 -0500

    adicionado e finalizado o desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/estruturaDeDadosBasico/adicionarItensEmUmArrayComPushUnshift.js
+++ algoritmosEestruturasDeDadosEmJavaScript/estruturaDeDadosBasico/adicionarItensEmUmArrayComPushUnshift.js
@@ -69,3 +69,31 @@ console.log(mixedNumbers(['IV', 5, 'six']));
 	  9
 	]
 */
+
+/*
+	Desafio
+	Definimos uma função, mixedNumbers, na qual estamos passando o array como
+	um argumento. Modifique a função usando push() e unshift() para adicionar
+	'I', 2, 'three' no início do array e 7, 'VIII', 9 ao final para que o array
+	retornado contenha a representação dos números de 1 a 9 em ordem.
+*/
+
+function mixedNumbers(arr) {
+  // Altere apenas o código abaixo desta linha
+  arr.unshift('I', 2, 'three');
+  arr.push(7, 'VIII', 9);
+  // Altere apenas o código acima desta linha
+  return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
+
+/*
+	[
+	  'I',     2,
+	  'three', 'IV',
+	  5,       'six',
+	  7,       'VIII',
+	  9
+	]
+*/
