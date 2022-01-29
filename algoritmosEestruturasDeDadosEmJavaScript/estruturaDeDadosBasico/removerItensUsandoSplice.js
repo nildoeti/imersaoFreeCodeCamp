797e8b8f3149d3569314b2271d7b9e5ad5ab00d6
Nildo <nildo@webstack.com.br>
2022-01-29 18:47:51 -0500

    adicioando e solucionado o desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/estruturaDeDadosBasico/removerItensUsandoSplice.js
+++ algoritmosEestruturasDeDadosEmJavaScript/estruturaDeDadosBasico/removerItensUsandoSplice.js
@@ -44,3 +44,15 @@ console.log('Antes: ', array); // Antes:  [ 'today', 'was', 'not', 'so', 'great'
 let newArray = array.splice(3, 2);
 console.log(newArray); // [ 'so', 'great' ]
 
+
+/*
+	Desafio
+	Iniciamos um array arr. Use splice() para remover elementos do arr, para
+	que apenas contenha elementos que somam ao valor de 10.
+*/
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Altere apenas o código abaixo desta linha
+arr.splice(1, 4);
+// Altere apenas o código acima desta linha
+console.log(arr);
