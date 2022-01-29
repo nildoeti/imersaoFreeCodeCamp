cbb9724cf2663b5245793f40344cfb19b7c4e018
Nildo <nildo@webstack.com.br>
2022-01-29 18:19:54 -0500

    adicionado exemplos práticos para pop()

---
+++ algoritmosEestruturasDeDadosEmJavaScript/estruturaDeDadosBasico/removerItensDeUmArrayComPopShift.js
@@ -0,0 +1,42 @@
+/*
+	Remover itens de um array com pop() e shift()
+
+	Tanto push() e unshift() possuem métodos correspondentes que são quase
+	opostos funcionais: pop() e shift(). Como você já pode ter adivinhado, em
+	vez de adicionar, pop() remove um elemento do fim de um array, enquanto
+	shift() remove um elemento do início. A diferença chave entre pop() e
+	shift() e seus primos push() e unshift(), é que nenhum dos métodos recebe
+	parâmetros. Cada um deles só permite que seja modificado um elemento por
+	vez em um array.
+
+	Vamos dar uma olhada:
+
+	let greetings = ['whats up?', 'hello', 'see ya!'];
+
+	greetings.pop();
+
+	greetings teria o valor ['whats up?', 'hello'].
+
+	greetings.shift();
+
+	greetings teria o valor ['hello'].
+
+	Nós também podemos retornar o valor do elemento removido com qualquer
+	método dessa forma:
+
+	let popped = greetings.pop();
+
+	greetings teria o valor [] e popped teria o valor hello.
+
+*/
+
+// exemplo 1
+let greetings = ['whats up?', 'hello', 'see ya!'];
+console.log('Antes: ', greetings); // Antes:  [ 'whats up?', 'hello', 'see ya!' ]
+
+greetings.pop();
+console.log('Depois: ', greetings); // Depois:  [ 'whats up?', 'hello' ]
+
+let popped = greetings.pop();
+console.log(popped); // hello
+console.log(greetings); // [ 'whats up?' ]
