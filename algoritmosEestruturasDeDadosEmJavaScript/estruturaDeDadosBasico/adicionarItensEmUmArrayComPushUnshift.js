c0a93566f0b2ff71640ee7e4636917207d705c53
Nildo <nildo@webstack.com.br>
2022-01-29 12:04:15 -0500

    adicionado e solucionado o desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/estruturaDeDadosBasico/adicionarItensEmUmArrayComPushUnshift.js
+++ algoritmosEestruturasDeDadosEmJavaScript/estruturaDeDadosBasico/adicionarItensEmUmArrayComPushUnshift.js
@@ -40,3 +40,32 @@ console.log('Depois: ', romanNumerals); // Depois:  [ 'XIX', 'XXI', 'XXI', 'XXII
 
 romanNumerals.unshift('Depois do depois: ', twentyThree);
 console.log(romanNumerals); // [ 'Depois do depois: ', 'XXIII', 'XIX', 'XXI', 'XXI', 'XXII' ]
+
+
+/*
+	Desafio
+	Definimos uma função, mixedNumbers, na qual estamos passando o array como
+	um argumento. Modifique a função usando push() e unshift() para adicionar
+	'I', 2, 'three' no início do array e 7, 'VIII', 9 ao final para que o array
+	retornado contenha a representação dos números de 1 a 9 em ordem.
+*/
+
+function mixedNumbers(arr) {
+  // Altere apenas o código abaixo desta linha
+  arr.unshift('I', 2, 'three');
+  arr.push(7, 'VIII', 9);
+  // Altere apenas o código acima desta linha
+  return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
+/*
+	Saida para exemplo do desafio do projeto
+	[
+	  'I',     2,
+	  'three', 'IV',
+	  5,       'six',
+	  7,       'VIII',
+	  9
+	]
+*/
