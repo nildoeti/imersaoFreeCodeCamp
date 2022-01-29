bd882863e33d3a3e1c2e468c0ec61a46e6d2171e
Nildo <nildo@webstack.com.br>
2022-01-28 19:21:30 -0500

    exemplo multidimensional  para array adicionado ao projeto

--- algoritmosEestruturasDeDadosEmJavaScript/depuracao/usarUmArrayParaArmazenarUmaColecaoDeDados.js
+++ algoritmosEestruturasDeDadosEmJavaScript/depuracao/usarUmArrayParaArmazenarUmaColecaoDeDados.js
@@ -47,6 +47,30 @@ let complexArray = [
 */
 
 // exemplo 1 para array simples
-let simpleArray = ['one', 2, true, false, undefined, null];
-console.log(simpleArray.length); // 6
+let simpleArray = ['one', 2, 'three', true, false, undefined, null];
+console.log(simpleArray.length); // 7
+
+// exemplo de array multidimensional
+let complexArray = [
+	[
+		{
+			one: 1,
+			two: 2
+		},
+		{
+			three: 3,
+			four: 4
+		}
+	],
+	[
+		{
+			a: 'a',
+			b: 'b'
+		},
+		{
+			c: 'c',
+			d: 'd'
+		}
+	]
+];
 
