364c3beaf7572ce1f2fe9d723f4415f7664b1fc7
Nildo <nildo@webstack.com.br>
2022-01-29 18:31:02 -0500

    adicionado e solucionado o desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/estruturaDeDadosBasico/removerItensDeUmArrayComPopShift.js
+++ algoritmosEestruturasDeDadosEmJavaScript/estruturaDeDadosBasico/removerItensDeUmArrayComPopShift.js
@@ -43,4 +43,27 @@ console.log(greetings); // [ 'whats up?' ]
 
 // exemplo 2 shift
 greetings.shift();
-console.log(greetings)
+console.log(greetings); // []
+
+
+
+/*
+	Desafio
+	Nós definimos uma função, popShift, a qual recebe um array como argumento e
+	retorna um novo array. Modifique a função, usando pop() e shift(), para
+	remover o primeiro e o último elemento do array passado como argumento, e
+	atribua os valores removidos para suas variáveis correspondentes, para que
+	o array retornado contenha seus valores.
+*/
+
+function popShift(arr) {
+	// let popped; // Altere esta linha
+	// let shifted; // Altere esta linha
+
+	let popped = arr.pop();
+	let shifted = arr.shift();
+
+	return [shifted, popped];
+}
+
+console.log(popShift(['challenge', 'is', 'not', 'complete']));
