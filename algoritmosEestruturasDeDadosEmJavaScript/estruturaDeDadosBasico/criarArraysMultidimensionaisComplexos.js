d1ac15671cb8e731ec6034667fe4a883a85b257b
Nildo <nildo@webstack.com.br>
2022-01-31 19:14:03 -0500

    solução 2 adicionado ao desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/estruturaDeDadosBasico/criarArraysMultidimensionaisComplexos.js
+++ algoritmosEestruturasDeDadosEmJavaScript/estruturaDeDadosBasico/criarArraysMultidimensionaisComplexos.js
@@ -103,10 +103,17 @@ let myNestedArray = [
 
 
   // solucao 1
-  ["unshift", false, 1, 2, 3, "complex", "nested"],
-  ["loop", "shift", 6, 7, 1000, "method"],
-  ["concat", false, true, "spread", "array", ["deep"]],
-  ["mutate", 1327.98, "splice", "slice", "push", [["deeper"]]],
-  ["iterate", 1.3849, 7, "8.4876", "arbitrary", "depth", [[["deepest"]]]]
+  // ["unshift", false, 1, 2, 3, "complex", "nested"],
+  // ["loop", "shift", 6, 7, 1000, "method"],
+  // ["concat", false, true, "spread", "array", ["deep"]],
+  // ["mutate", 1327.98, "splice", "slice", "push", [["deeper"]]],
+  // ["iterate", 1.3849, 7, "8.4876", "arbitrary", "depth", [[["deepest"]]]]
+
+  // solucao 2
+   'level 1',                   /* myNestedArray[0]            */
+  ['level 2'],	               /* myNestedArray[1][0]          */
+  [['level 3','deep']],	       /* myNestedArray[2][0][0]       */
+  [[['level 4','deeper']]],    /* myNestedArray[3][0][0][0]    */
+  [[[['level 5','deepest']]]]  /* myNestedArray[4][0][0][0][0] */
 ];
 
