e2872cde6cc74d1b8e06b12ae045403d221ee6e6
Nildo <nildo@webstack.com.br>
2022-02-02 20:39:41 -0500

    adicionado solução 1 ao projeto

--- algoritmosEestruturasDeDadosEmJavaScript/programacaoDeAlgoritmosBasicos/inverterUmaString.js
+++ algoritmosEestruturasDeDadosEmJavaScript/programacaoDeAlgoritmosBasicos/inverterUmaString.js
@@ -1,1 +1,17 @@
-inverterUmaString.js
+/*
+	Inverter uma string
+
+	Inverta a string fornecida.
+	Você pode ter que transformar a string em um array antes de poder inverter.
+	Seu resultado deve ser uma string.
+
+*/
+
+// solução 1
+function reverseString(str) {
+	let reversedStr = "";
+	for (let i = str.length - 1; i >= 0; i--) {
+		reversedStr += str[i];
+	}
+	return reversedStr;
+}
