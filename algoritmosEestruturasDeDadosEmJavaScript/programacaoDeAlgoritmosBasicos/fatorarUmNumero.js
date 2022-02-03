a2fd6eaf7f16b25f8f251059c20f28d52891be9e
Nildo <nildo@webstack.com.br>
2022-02-02 20:53:41 -0500

    adicioando solução 1 ao projeto

---
+++ algoritmosEestruturasDeDadosEmJavaScript/programacaoDeAlgoritmosBasicos/fatorarUmNumero.js
@@ -0,0 +1,24 @@
+/*
+	Fatorar um número
+
+	Retorne o fatorial do inteiro fornecido.
+	Se o inteiro é representado com a letra n, o fatorial é o produto de todos
+	os inteiros positivos menores ou iguais a n.
+	Fatoriais são frequentemente representados com a notação abreviada n!
+
+	Por exemplo: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+	Apenas números inteiros maiores ou iguais a zero serão fornecidos para a
+	função.
+
+*/
+// solução 1
+// function factorialize(num) {
+//   let product = 1;
+//   for (let i = 2; i <= num; i++) {
+//     product *= i;
+//   }
+//   return product;
+// }
+
+
