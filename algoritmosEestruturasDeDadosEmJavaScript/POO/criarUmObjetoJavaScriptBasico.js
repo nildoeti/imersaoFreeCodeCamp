01d2ed5d489ae7a7daf23a11c70be0d081978009
Nildo <nildo@webstack.com.br>
2022-02-07 20:54:58 -0500

    adicionado exemplo de objeto em javascript

---
+++ algoritmosEestruturasDeDadosEmJavaScript/POO/criarUmObjetoJavaScriptBasico.js
@@ -0,0 +1,34 @@
+/*
+    Criar um objeto JavaScript básico
+
+    Pense sobre as coisas que as pessoas veem todos os dias, como carros,
+    lojas e pássaros. Tudo isso são objetos: coisas tangíveis com que pessoas
+    podem observar e interagir.
+
+    Quais são algumas qualidades destes objetos? Um carro possui rodas. Lojas
+    vendem itens. Pássaros possuem asas.
+
+    Estas qualidades, ou propriedades, definem o que faz um objeto. Note que
+    objetos similares compartilham de propriedades iguais, mas podem ter
+    diferentes valores para estas propriedades. Por exemplo, todos os carros
+    possuem rodas, mas nem todos os carros possuem o mesmo número de rodas.
+
+    Objetos em JavaScript são utilizados como modelos de objetos do mundo real,
+    dando a eles propriedades e comportamentos assim como se fossem análogos ao
+    mundo real. Aqui está um exemplo utilizando estes conceitos para a criação
+    de um objeto duck:
+
+    let duck = {
+      name: "Aflac",
+      numLegs: 2
+    };
+
+    O objeto duck possui dois pares de propriedades/valores: um name sendo
+    Aflac e um numLegs sendo 2.
+*/
+
+// exemplo de um objeto em JavaScript
+let duck = {
+    name: 'Aflac',
+    numLegs: 2
+};
