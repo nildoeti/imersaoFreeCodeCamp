db6a54d0925556b0fff563c6c1aecca0f20cea8a
Nildo <nildo@webstack.com.br>
2022-02-07 19:30:27 -0500

    Desafio do projeto atualizado

--- algoritmosEestruturasDeDadosEmJavaScript/programacaoDeAlgoritmosBasicos/encontrarPalavraMaisLongaEmUmaString.js
+++ algoritmosEestruturasDeDadosEmJavaScript/programacaoDeAlgoritmosBasicos/encontrarPalavraMaisLongaEmUmaString.js
@@ -7,18 +7,24 @@
 
 // solução 1
 function findLongestWordLength(str) {
-  let words = str.split(' ');
-  let maxLength = 0;
+	// solução 1
 
-  for (let i = 0; i < words.length; i++) {
-    if (words[i].length > maxLength) {
-      maxLength = words[i].length;
-    }
-  }
+	// let words = str.split(" ");
+	// let maxLength = 0;
 
-  return maxLength;
+	// for (let i = 0; i < words.length; i++) {
+	// 	if (words[i].length > maxLength) {
+	// 		maxLength = words[i].length;
+	// 	}
+	// }
+
+	// return maxLength;
+
+	// solução 2
+	return s.split(' ')
+	.reduce(function(longest, word) {
+		return Math.max(longest, word.length);
+	}, 0);
 }
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
-
-
