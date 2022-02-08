610a948332eef37d1513a44f8539082e4e4fcca9
Nildo <nildo@webstack.com.br>
2022-02-07 19:48:03 -0500

    adicionado solução 1 ao projeto

---
+++ algoritmosEestruturasDeDadosEmJavaScript/programacaoDeAlgoritmosBasicos/confirmarFinal.js
@@ -0,0 +1,20 @@
+/*
+    Confirmar o final
+
+    Verifique se uma string (primeiro argumento, str) termina com a sequência
+    de caracteres de destino fornecida (segundo argumento, target).
+
+    Este desafio pode ser resolvido com o método .endsWith(), que foi
+    introduzido na ES2015. Para este desafio, entretanto, gostaríamos que
+    você usasse um dos métodos de substring JavaScript.
+*/
+
+// solução 1
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+
+  return str.slice(str.length - target.length) === target;
+}
+
+confirmEnding("He has to give me a new name", "name");
