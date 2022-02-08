6bd73fa8606026a6d60d52b79a1dd7e3e44d3649
Nildo <nildo@webstack.com.br>
2022-02-07 19:40:29 -0500

    adicionado solução 1 ao projeto

---
+++ algoritmosEestruturasDeDadosEmJavaScript/programacaoDeAlgoritmosBasicos/retornarOsMaioresNumerosEmArrays.js
@@ -0,0 +1,24 @@
+/*
+    Retornar os maiores números em arrays
+
+    Retorna um array que consiste no maior número de cada sub-array fornecido.
+    Por simplicidade, o array fornecido conterá exatamente 4 sub-arrays.
+
+    Lembre-se: você pode iterar através de um array com um loop simples, e
+    acesse cada membro com a sintaxe de array arr[i].
+*/
+
+// solução 1
+function largesOfFour(arr) {
+    let results = [];
+    for (let i = 0; i < arr.length; i++) {
+        let largestNumber = arr[i][0];
+        for (let j =1; j < arr[i.length]; j++) {
+            if (arr[i][j] > largestNumber) {
+                largestNumber = arr[i][j];
+            }
+        }
+        results[i] = largestNumber;
+    }
+    return results;
+}
