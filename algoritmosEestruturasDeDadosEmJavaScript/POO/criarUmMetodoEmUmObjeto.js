7d67675b237c5abebdfa4d9f3971648f72d03390
Nildo <nildo@webstack.com.br>
2022-02-09 20:54:43 -0300

    adicionado e sulucionado o desafio do projeto

--- algoritmosEestruturasDeDadosEmJavaScript/POO/criarUmMetodoEmUmObjeto.js
+++ algoritmosEestruturasDeDadosEmJavaScript/POO/criarUmMetodoEmUmObjeto.js
@@ -43,3 +43,19 @@ let pessoa = {
 
 let pessoaNomeCompleto = pessoa.nomeCompleto();
 console.log(pessoaNomeCompleto); // Fulano de Tal
+
+
+/*
+    Desafio
+    Usando o objeto dog, de a ele um método chamado sayLegs. O método deve
+    retornar a frase This dog has 4 legs. (Este cachorro possui 4 pernas.)
+*/
+
+let dog = {
+    name: 'Spot',
+    numLegs: 4,
+    sayLegs: function() {
+        return "This dog has 4 legs."
+    }
+}
+dog.sayLegs;
