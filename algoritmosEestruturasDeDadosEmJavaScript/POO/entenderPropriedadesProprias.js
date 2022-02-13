22ad2c35d795cdf014359178bcf25d8805043f24
Nildo <nildo@webstack.com.br>
2022-02-13 19:14:45 -0300

    adicionado exemplo de propriedades proprias ao projeto

--- algoritmosEestruturasDeDadosEmJavaScript/POO/entenderPropriedadesProprias.js
+++ algoritmosEestruturasDeDadosEmJavaScript/POO/entenderPropriedadesProprias.js
@@ -1,1 +1,56 @@
-entenderPropriedadesProprias.js
+/*
+	Entender propriedades próprias
+
+	No próximo exemplo, o construtor de Bird define duas propriedades: name e
+	numLegs:
+
+	function Bird(name) {
+	  this.name = name;
+	  this.numLegs = 2;
+	}
+
+	let duck = new Bird("Donald");
+	let canary = new Bird("Tweety");
+
+	name e numLegs são chamados own properties, pois são definidos diretamente
+	na instância do objeto. Isso significa que cada duck e canary possuem suas
+	próprias cópias separadas destas propriedades. Na verdade, toda instância
+	de Bird terá sua própria cópia dessas propriedades. O código a seguir
+	adiciona todas as propriedades próprias (own properties) de duck para o
+	array ownProps:
+
+	let ownProps = [];
+
+	for (let property in duck) {
+	  if(duck.hasOwnProperty(property)) {
+	    ownProps.push(property);
+	  }
+	}
+
+	console.log(ownProps);
+
+	O console vai exibir o valor ["name", "numLegs"].
+
+*/
+
+function Bird (name) {
+	this.name = name;
+	let numLegs = 2;
+}
+
+let duck = new Bird('Donald');
+let canary = new Bird('Tweety');
+
+console.log(duck); // Bird { name: 'Donald' }
+console.log(canary); // Bird { name: 'Tweety' }
+
+
+let ownProps = [];
+for (let property in duck) {
+	if (duck.hasOwnProperty(property)) { // hasOwnProperty retorna um boolean
+											// no objeto especificado
+		ownProps.push(duck);
+	}
+}
+
+console.log(ownProps);
