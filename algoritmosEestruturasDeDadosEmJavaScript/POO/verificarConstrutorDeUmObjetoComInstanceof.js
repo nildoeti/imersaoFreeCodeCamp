a394332043e7c0732f4f291d66d841e9721c3cf4
Nildo <nildo@webstack.com.br>
2022-02-13 14:08:44 -0300

    Verificar o construtor de um objeto com instanceof foi finalizado

---
+++ algoritmosEestruturasDeDadosEmJavaScript/POO/verificarConstrutorDeUmObjetoComInstanceof.js
@@ -0,0 +1,47 @@
+/*
+	Verificar o construtor de um objeto com instanceof
+
+	Toda vez que a função construtora cria um novo objeto, o objeto é definido
+	como uma instance do seu construtor. JavaScript provê uma forma conveniente
+	para verificar isso com o operador instanceof. instanceof permite que você
+	compare um objeto a um construtor, retornando true ou false caso seja ou
+	não um objeto criado pelo construtor, respectivamente. Exemplo:
+
+	let Bird = function(name, color) {
+	  this.name = name;
+	  this.color = color;
+	  this.numLegs = 2;
+	}
+
+	let crow = new Bird("Alexis", "black");
+
+	crow instanceof Bird;
+
+	Este método instanceof irá retornar true.
+
+	Se um objeto for criado sem usar um construtor, instanceof verificará que
+	não é uma instância daquele construtor:
+
+	let canary = {
+	  name: "Mildred",
+	  color: "Yellow",
+	  numLegs: 2
+	};
+
+	canary instanceof Bird;
+
+	Este método instanceof irá retornar false.
+
+*/
+
+// 	Verificar o construtor de um objeto com instanceof
+let Bird = 	function (name, color) {
+	this.name = name;
+	this.color = color;
+	this.numLegs = 2;
+};
+
+let crow = new Bird('Alexis', 'black');
+crow instanceof Bird;
+
+console.log(crow); // Bird { name: 'Alexis', color: 'black', numLegs: 2 }
