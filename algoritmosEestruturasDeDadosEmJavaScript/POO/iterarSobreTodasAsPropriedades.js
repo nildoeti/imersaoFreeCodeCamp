23a61d0846c8b89e83b824bb8c596fe8f06b2ffe
Nildo <nildo@webstack.com.br>
2022-02-14 07:31:48 -0300

    adicionado exemplos para own property e prototype property

--- algoritmosEestruturasDeDadosEmJavaScript/POO/iterarSobreTodasAsPropriedades.js
+++ algoritmosEestruturasDeDadosEmJavaScript/POO/iterarSobreTodasAsPropriedades.js
@@ -1,1 +1,47 @@
-iterarSobreTodasAsPropriedades.js
+/*
+	Iterar sobre todas as propriedades
+
+	Até agora você já viu dois tipos de propriedades: as propriedades own
+	properties e prototype. Propriedades próprias (ou Own properties) são
+	definidas diretamente na própria instância do objeto. E as propriedades do
+	protótipo são definidas em prototype.
+
+	function Bird(name) {
+	  this.name = name;  //own property
+	}
+
+	Bird.prototype.numLegs = 2; // prototype property
+
+	let duck = new Bird("Donald");
+
+	Aqui está como você adiciona own properties duck para o array ownProps e
+	propriedades prototype para o array prototypeProps:
+
+	let ownProps = [];
+	let prototypeProps = [];
+
+	for (let property in duck) {
+	  if(duck.hasOwnProperty(property)) {
+	    ownProps.push(property);
+	  } else {
+	    prototypeProps.push(property);
+	  }
+	}
+
+	console.log(ownProps);
+	console.log(prototypeProps);
+
+	console.log(ownProps) deve exibir no console ["name"], e
+	console.log(prototypeProps) exibirá no console ["numLegs"].
+
+*/
+
+function Bird (name) {
+	this.name = name; // own property
+}
+
+Bird.prototype.numLegs = 2; // prototype property
+
+let duck = new Bird('Donald');
+
+console.log(duck);
