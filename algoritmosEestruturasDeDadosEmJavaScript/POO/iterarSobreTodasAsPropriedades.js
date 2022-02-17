0ee3b1465ca5b354b8ee740f63e1d095eddbcbb2
Nildo <nildo@webstack.com.br>
2022-02-16 22:04:23 -0300

    adicionado exemplos para interação com todas as propriedades own property e prototype property

--- algoritmosEestruturasDeDadosEmJavaScript/POO/iterarSobreTodasAsPropriedades.js
+++ algoritmosEestruturasDeDadosEmJavaScript/POO/iterarSobreTodasAsPropriedades.js
@@ -1,63 +1,68 @@
 /*
-    Iterar sobre todas as propriedades
+	Iterar sobre todas as propriedades
 
-    Até agora você já viu dois tipos de propriedades: as propriedades own
-    properties e prototype. Propriedades próprias (ou Own properties) são
-    definidas diretamente na própria instância do objeto. E as propriedades do
-    protótipo são definidas em prototype.
+	Até agora você já viu dois tipos de propriedades: as propriedades own
+	properties e prototype. Propriedades próprias (ou Own properties) são
+	definidas diretamente na própria instância do objeto. E as propriedades do
+	protótipo são definidas em prototype.
 
-    function Bird(name) {
-    this.name = name;  //own property
-    }
+	function Bird(name) {
+	  this.name = name;  //own property
+	}
 
-    Bird.prototype.numLegs = 2; // prototype property
+	Bird.prototype.numLegs = 2; // prototype property
 
-    let duck = new Bird("Donald");
+	let duck = new Bird("Donald");
 
-    Aqui está como você adiciona own properties duck para o array ownProps e
-    propriedades prototype para o array prototypeProps:
+	Aqui está como você adiciona own properties duck para o array ownProps e
+	propriedades prototype para o array prototypeProps:
 
-    let ownProps = [];
-    let prototypeProps = [];
+	let ownProps = [];
+	let prototypeProps = [];
 
-    for (let property in duck) {
-    if(duck.hasOwnProperty(property)) {
-        ownProps.push(property);
-    } else {
-        prototypeProps.push(property);
-    }
-    }
+	for (let property in duck) {
+	  if(duck.hasOwnProperty(property)) {
+	    ownProps.push(property);
+	  } else {
+	    prototypeProps.push(property);
+	  }
+	}
 
-    console.log(ownProps);
-    console.log(prototypeProps);
+	console.log(ownProps);
+	console.log(prototypeProps);
 
-    console.log(ownProps) deve exibir no console ["name"], e
-    console.log(prototypeProps) exibirá no console ["numLegs"].
+	console.log(ownProps) deve exibir no console ["name"], e
+	console.log(prototypeProps) exibirá no console ["numLegs"].
 
 */
+
 // exemplos para own property e prototype property
 function Bird (name) {
-    this.name = name; // own propert
+	this.name = name; // own property
 }
 
 Bird.prototype.numLegs = 2; // prototype property
 
-let duck = new Bird('Donald');
+let duck = new Bird('Donald'); Bird { name: 'Donald' }
+
 console.log(duck);
 
 
-//  adiciona own properties duck para o array ownProps e propriedades prototype
-// para o array prototypeProps
-let ownProperty = [];
+
+// adiciona own properties duck para o array ownProps e propriedades 
+// prototype para o array prototypeProps
+
+let ownProps = [];
 let prototypeProps = [];
 
 for (let property in duck) {
-    if (duck.hasOwnProperty(property)) {
-        ownProperty.push(property);
-    } else {
-        prototypeProps.push(property);
-    }
+	if (duck.hasOwnProperty(property)) {
+		ownProps.push(property);
+	} else {
+		prototypeProps.push(property);
+	}
 }
 
-console.log(ownProperty);
-console.log(prototypeProps);
+console.log(ownProps); // [ 'name' ]
+console.log(prototypeProps); // [ 'numLegs' ]
+
