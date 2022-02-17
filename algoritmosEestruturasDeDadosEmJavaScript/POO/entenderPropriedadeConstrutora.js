5e0767ee42dd0f7a9f9ba7c477150de12fc1d194
Nildo <nildo@webstack.com.br>
2022-02-17 15:31:52 -0300

    adicionado exemplo para verificar qual a pripriedade do objeto

---
+++ algoritmosEestruturasDeDadosEmJavaScript/POO/entenderPropriedadeConstrutora.js
@@ -0,0 +1,55 @@
+/*
+	Entender a propriedade construtora
+
+	Tem uma propriedade especial do constructor localizada nas instâncias dos
+	objetos duck e beagle que foram criados nos desafios anteriores:
+
+	let duck = new Bird();
+	let beagle = new Dog();
+
+	console.log(duck.constructor === Bird);
+	console.log(beagle.constructor === Dog);
+
+	Ambas as chamadas a console.log vão exibir true no console.
+
+	Note que a propriedade constructor é uma referência a função construtora
+	que criou a instância. A vantagem da propriedade constructor é que se torna
+	possível verificar essa propriedade para descobrir qual o tipo do objeto.
+	Aqui está um exemplo de como isso poderia ser utilizado:
+
+	function joinBirdFraternity(candidate) {
+	  if (candidate.constructor === Bird) {
+	    return true;
+	  } else {
+	    return false;
+	  }
+	}
+
+	Observação: já que a propriedade constructor pode ser sobrescrita (o que
+	será abordado nos próximos dois desafios), geralmente é melhor utilizar o
+	método instanceof para verificar o tipo de um objeto.
+
+*/
+
+// exemplo 1
+function Bird(name) {
+	this.name = name;
+}
+
+Bird.prototype.numLegs = 2; // own property
+
+// criar nova instancia
+let duck = new Bird('Donald');
+
+// console.log(duck.constructor === Bird); // true
+
+
+// verificar essa propriedade para descobrir qual o tipo do objeto}
+function joinBirdFraternity (candidate) {
+	if (candidate.constructor === Bird) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
